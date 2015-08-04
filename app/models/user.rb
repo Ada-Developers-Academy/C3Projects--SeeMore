bfdadb705fad561fdf59df4cdba4fa0288843e5f
Lila Garcia <lilagrc@gmail.com>
2015-08-04 16:28:07 -0700

    Added User method to find or create user from auth_has

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+
+
+  def self.find_or_create_user(auth_hash)
+    uid = auth_hash["uid"]
+    provider = auth_hash["provider"]
+
+    user = User.where(uid: uid, provider: provider).first_or_initialize
+    user.email = auth_hash["info"]["email"]
+    user.name = auth_hash["info"]["name"]
+
+    return user.save ? user : nil
+  end
 end
