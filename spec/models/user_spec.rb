26f1a691db0d2c3ce32dd8bf6630fe1eb051686c
Carly Jugler <carjug@gmail.com>
2015-08-04 16:43:07 -0700

    Adds additional rspec validation tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,18 +19,39 @@ RSpec.describe User, type: :model do
         user.name = nil
 
         expect(user).to be_invalid
+        expect(user.errors.keys).to include(:name)
       end
 
       it "requires a uid" do
         user.uid = nil
 
         expect(user).to be_invalid
+        expect(user.errors.keys).to include(:uid)
       end
 
       it "requires a provider" do
         user.provider = nil
 
         expect(user).to be_invalid
+        expect(user.errors.keys).to include(:provider)
+      end
+
+      it "name has to be unique" do
+        user.save
+
+        user2 = User.create(name: "Ada")
+
+        expect(user2).to be_invalid
+        expect(user2.errors.keys).to include(:name)
+      end
+
+      it "uid has to be unique" do
+        user.save
+
+        user2 = User.create(uid: "12354")
+
+        expect(user2).to be_invalid
+        expect(user2.errors.keys).to include(:uid)
       end
     end
 end
