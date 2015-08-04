f09f72c23908d8968a809362f67a1cd429032ff3
Elia <elgrenier@gmail.com>
2015-08-04 15:37:58 -0700

    wip: omniauth developer csrf workaround, for development ONLY

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+     
+    post "/auth/:provider/callback", to: "sessions#create"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
