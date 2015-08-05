81077fb5bdbcc14381353ac4319c977e0fc8bccc
Loraine <loraine@lorainekv.com>
2015-08-05 13:42:12 -0700

    Started on the show view.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,13 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    auth_hash = request.env['omniauth.auth'] 
+    auth_hash = request.env['omniauth.auth']
     # User.find_or_create_from_omniauth(auth_hash)
 
     # redirect_to root_path
   end
+
+  def show
+  end
+  
 end
