3ad9917592c3d49f2a0297c187ddf3c933b8c3c5
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-05 15:44:26 -0700

    created TwitterClient class to initialize client

---
+++ lib/twitter_client.rb
@@ -0,0 +1,16 @@
+class TwitterClient
+  attr_reader :client
+
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+    end
+  end
+
+  # def user_search(user)
+  #   # self.client.search ...
+  # end
+end
