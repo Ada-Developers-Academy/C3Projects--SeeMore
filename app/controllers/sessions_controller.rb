504f5f4f7fa07e5cd0d1a3ada5fbfc24a8cfc56b
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-05 14:19:18 -0700

    Changes method name to from_omnihash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => :create
 
   def create
-    @user = User.find_or_create_from_auth_hash(auth_hash)
+    @user = User.find_or_create_from_omniauth(auth_hash)
     session[:user_id] = @user.id
     # raise
     redirect_to '/'
