19c2818c86eea477f8cb7cae216008d8ec6085ee
MisShellyMac <michellemccarthy@hotmail.com>
2015-08-05 10:20:19 -0700

    fixed the developer environment part for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,7 @@
 class SessionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :create
+  def create
+    auth_hash = request.env['omniauth.auth']
+    raise
+  end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,3 @@
-# module OmniAuth
-#   module Strategies
-#     autoload :Developer, Rails.root.join('lib', 'strategies', 'developer')
-#   end
-# end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer,
-    :fields => [:username, :email],
-    :uid_field => :email
+  provider :developer
 end
