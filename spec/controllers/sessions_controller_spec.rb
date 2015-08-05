db3a32883ad3dd855a0f135a2ea21ef7c12c77db
Corinne Pingul <corinnepingul@gmail.com>
2015-08-05 16:23:50 -0700

    WIP - tests for session controller specs.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe "GET #create" do
+    context "when using developer strategies" do
+
+    end
+
+    context "when using twitter authentication" do
+      context "is successful" do
+        before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter] }
+
+        it "redirects to the homepage" do
+          get :create, provider: :twitter
+          expect(response).to redirect_to root_path
+        end
+
+        it "creates a user" do
+           expect { get :create, provider: :twitter }.to change(Stalker, :count).by(1)
+        end
+
+        it "assigns session[:user_id]" do
+          get :create, provider: :twitter
+          expect(assigns(:user)).to be_an_instance_of Stalker
+        end
+      end
+
+      context "user already exists" do
+        before(:each) do
+          request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+          get :create, provider: :twitter
+        end
+        # before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter] }
+        let(:user) { Stalker.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:twitter]) }
+
+        it "doesn't create a new user" do
+          get :create, provider: :twitter
+
+          expect(Stalker.count).to eq 1
+        end
+
+        it "assigns session[:user_id]" do
+          get :create, provider: :twitter
+
+          expect(session[:user_id]).to eq user.id
+        end
+      end
+
+      context "when failing to save the user" do
+        before { request.env["omniauth.auth"] = {
+          uid: "1234",
+          info: {}
+          }
+        }
+
+       it "redirect to home with flash error" do
+         get :create, provider: :twitter
+         expect(response).to redirect_to root_path
+         expect(flash[:notice]).to include "Failed to save the user"
+       end
+      end
+    end
+
+    context "when using vimeo authentication" do
+
+    end
+
+    context "when using instagram authentication" do
+
+    end
+  end
 
 end
