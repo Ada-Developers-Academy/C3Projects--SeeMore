53e5cb4e58b822431252393bc0d309385b6ebb68
Corinne Pingul <corinnepingul@gmail.com>
2015-08-05 13:59:10 -0700

    Sets up login via vimeo.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer
   provider :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"]
+  provider :vimeo, ENV["VIMEO_CLIENT_ID"], ENV["VIMEO_CLIENT_SECRET"]
 end
