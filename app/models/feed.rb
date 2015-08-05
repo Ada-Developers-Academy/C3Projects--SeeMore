179f291110e4cc9dbe116267e16e2bde13fbbdba
Brenna <brennaleker@gmail.com>
2015-08-05 15:30:46 -0700

    fixed typo in feed model

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,5 +1,5 @@
 class Feed < ActiveRecord::Base
   # Associations--------------------------------------------
-  has_and_belongs_to_many :authetnicated_users
+  has_and_belongs_to_many :au_users
   has_many :posts
 end
