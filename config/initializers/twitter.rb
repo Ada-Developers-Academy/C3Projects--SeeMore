4ae1447542eca7b80d0a9d3adfd5db5782656b9c
Loraine <loraine@lorainekv.com>
2015-08-05 16:28:24 -0700

    Changing twitter to client in the initializer.

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-twitter = Twitter::REST::Client.new do |config|
+client = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['CONSUMER_KEY']
   config.consumer_secret = ENV['CONSUMER_SECRET']
 end
