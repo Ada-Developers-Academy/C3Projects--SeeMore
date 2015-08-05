f0b715cb382f165f415a09cc43a75d08f2a04a31
Loraine <loraine@lorainekv.com>
2015-08-05 16:28:38 -0700

    Starting the feeds controller spec.

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe FeedsController, type: :controller do
 
+describe "GET feeds#search" do
+
+  it "loads the search form" do
+    get :search
+    expect(response).to render_template(:search)
+  end
+
+
+end
+
 end
