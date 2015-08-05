018d6b4e76746a32dae75c6f46e4ba1c4634c9a0
Carly Jugler <carjug@gmail.com>
2015-08-05 14:15:27 -0700

    Adds rspec test for create method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ skip_before_filter :verify_authenticity_token
       @user = User.find_or_create_user(auth_hash)
       if @user
         session[:user_id] = @user.id
-        raise
+
         redirect_to root_path
       else
         redirect_to root_path, notice: "Failed to save the user"
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe "GET #create" do
+    context "when using omniauth developer authorization" do
+      context "when successful" do
 
+        before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:developer] }
+
+        it "redirects to home page" do
+          get :create, provider: :developer
+
+          expect(response).to redirect_to root_path
+        end
+      end
+    end
+  end
 end
