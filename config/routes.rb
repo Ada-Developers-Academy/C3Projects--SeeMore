98b58fad44f37c4e9456fe3db019a6d25ffc64cd
Victoria <vikshab2010@hotmail.com>
2015-08-05 10:36:42 -0700

    Added httparty gem, Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post "/auth/developer/callback", to: "sessions#create"
+  get "/auth/:provider/callback", to: "sessions#create"
+
   delete "/logout", to: "sessions#destroy", as: "logout"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
