6b98be5a3d538f81f4260c6eca3f4db6831f8832
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-05 14:19:52 -0700

    WIP: user authorization tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   before(:each) do
     # resetting OmniAuth to a consistent state before tests
-    OmniAuth.config.mock_auth[:default] = nil
+    OmniAuth.config.mock_auth[:instagram] = nil
     # OmniAuth.config.mock_auth[:instagram] = nil
   end
 
@@ -13,44 +13,42 @@ RSpec.describe User, type: :model do
       it "is valid" do
         expect(zyn).to be_valid
       end
-  end
-    #   it "requires an email" do
-    #     user.email = nil
-    #     expect(user).to be_invalid
-    #   end
-    #
-    #   it "requires a username" do
-    #     user.username = nil
-    #     expect(user).to be_invalid
-    #   end
-    #
-    #   it "requires a uid" do
-    #     user.uid = nil
-    #     expect(user).to be_invalid
-    #   end
-    #
-    #   it "requires a provider" do
-    #     user.provider = nil
-    #     expect(user).to be_invalid
-    #   end
-    # end
-    #
-    #
-    # describe ".initialize_from_omniauth" do
-    #   let(:user) {
-    #     User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:github])
-    #     # mock_auth is like a result of an authentication from github
-    #   }
-    #
-    #   it "creates a valid user" do
-    #     expect(user).to be_valid
-    #   end
-    #
-    #   context "when it's invalid" do
-    #     it "returns nil" do
-    #       user = User.find_or_create_from_omniauth({"uid" => "123", "info" => {}})
-    #       expect(user).to be_nil
-    #     end
-    #   end
-    # end
+
+      it "requires a uid" do
+        zyn.uid = nil
+        expect(zyn).to be_invalid
+      end
+
+      it "requires a provider" do
+        zyn.provider = nil
+        expect(zyn).to be_invalid
+      end
+  end # validations
+
+
+    describe ".find_or_create_from_omniauth" do
+      # let(:petunia) {
+      #   User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:instagram])
+      #   # mock_auth is like a result of an authentication from github
+      # }
+
+      it "creates a valid user" do
+        binding.pry
+        petunia =   User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:instagram])
+        expect(petunia).to be_valid
+      end
+
+      context "when it's invalid" do
+        it "returns nil" do
+          user = User.find_or_create_from_omniauth({"uid" => "123", "info" => {}})
+          expect(user).to be_nil
+        end
+
+        it "returns nil" do
+          user = User.find_or_create_from_omniauth({"uid" => nil, "info" => {}})
+          expect(user).to be_nil
+        end
+
+      end
+    end
   end
