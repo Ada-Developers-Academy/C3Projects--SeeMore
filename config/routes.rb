25d7e744d34bc8feb92e928ef0bb6cba9462bd8c
amirahaile <amira.dhaile@gmail.com>
2015-08-05 11:30:46 -0700

    Added all our routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,15 @@
 Rails.application.routes.draw do
-   get  "/auth/developer", to: 'sessions#create'
-   delete "/logout", to: 'sessions#destroy', as: 'logout'
-   
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'users#index'
+  root 'sessions#new'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get    '/about',           to: 'sessions#show',    as: 'about'
+  get    '/auth/:provider',  to: 'sessions#create'
+  delete '/logout',          to: 'sessions#destroy', as: 'logout'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  # TODO: brownie points - add users#show, users#update
+  resources :users, only: [:create, :destroy]
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :feeds, except: [:destroy]
+  get '/feeds/search',         to: 'feeds#search',         as: 'search'
+  get '/feeds/search/results', to: 'feeds#search_results', as: 'search_results'
 end
