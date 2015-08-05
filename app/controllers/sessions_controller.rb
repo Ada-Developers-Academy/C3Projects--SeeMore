7a8dbb8028758c284550a67ebb957c479c5bd051
MisShellyMac <michellemccarthy@hotmail.com>
2015-08-05 10:39:22 -0700

    set up omniauth developers strategy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,19 @@ class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: :create
   def create
     auth_hash = request.env['omniauth.auth']
-    raise
+    if auth_hash["provider"] == "developer"
+      @user = User.find_or_create_from_omniauth(auth_hash)
+    elsif auth_hash["provider"] == "twitter"
+    elsif auth_hash["provider"] == "instagram"
+    else
+      redirect_to root_path, notice: "Failed to authenticate"
+    end
+
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to root_path, notice: "Failed to save the user"
+    end
   end
 end
--- app/models/stalker.rb
+++ app/models/stalker.rb
@@ -1,2 +1,16 @@
 class Stalker < ActiveRecord::Base
+  #validations
+  validates :username, presence: true
+  validates :uid, presence: true
+  validates :provider, presence: true
+
+    def self.find_or_create_from_omniauth(auth_hash)
+      uid = auth_hash["uid"]
+      provider = auth_hash["provider"]
+
+      user = User.where(uid: uid, provider: provider).first_or_initialize
+      user.username = auth_hash["info"]["name"]
+
+      return user.save ? user: nil
+    end
 end
