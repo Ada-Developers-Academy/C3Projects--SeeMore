c94b7803f0d72ab7a0102371e0bf56629fdc5fa0
marleigh <camarleigh@gmail.com>
2015-08-04 17:34:34 -0700

    greated sessions and user controllers and models. Also did initial auth/dev login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+   get  "/auth/developer", to: 'sessions#create'
+   delete "/logout", to: 'sessions#destroy', as: 'logout'
+   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
