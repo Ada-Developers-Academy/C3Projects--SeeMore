d830436f0014ff1c1de1a666d6f7187d0623113f
Ashley Watkins <AWatkins31@gmail.com>
2015-08-05 11:57:08 -0700

    Set up initial root path route + view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+
   get "/auth/:provider/callback", to: "sessions#create"
   post "/auth/:provider/callback", to: "sessions#create" # this is required for the OmniAuth Developer Strategy
   # get "/auth/:provider"
