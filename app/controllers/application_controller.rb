19e6f4408f6d59c2b14f8033f04e961dfa6acbd0
Shanna Chau <shannachau@gmail.com>
2015-08-05 16:50:34 -0700

    WIP - SO CLOSE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
+require 'twitterclient'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filer :set_twitter_client
+
+  def set_twitter_client
+    @twitter_client ||= TwitterClient.new
+  end
 end
