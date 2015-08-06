b8f4e72a887a5863118b375420a8f7e6b125381f
Anita <asovay@gmail.com>
2015-08-06 11:51:22 -0700

    added twitter API with search function, currently working on pulling feed

--- app/controllers/twitters_controller.rb
+++ app/controllers/twitters_controller.rb
@@ -1,16 +1,5 @@
 class TwittersController < ApplicationController
 
-  # search page: search for twitter user
-  # def search; end
-
-  # TWITTER_URI = "https://api.twitter.com/1.1"
-
-  # SEARCH_URI = "/users/search.json?"
-  # "q=#{username}"
-
-  # FEED_URI = "/statuses/user_timeline.json?"
-  # "user_id=#{provider_id}&screen_name=#{username}"
-
   # to refactor into application controller, accepting params
   def twitter_redirect
     if params[:username].present?
@@ -23,8 +12,6 @@ class TwittersController < ApplicationController
   def search
     @username = params[:username]
     @response = @twitter.client.user_search("#{@username}")
-
-    # response = HTTParty.get(TWITTER_URI + SEARCH_URI + "q=#{@username}")
   end
 
 end
