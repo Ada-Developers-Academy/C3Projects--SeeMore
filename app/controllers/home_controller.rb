4534de87ff9688fcb5d43a8660ad9f7e8cb2aec2
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-06 13:54:42 -0700

    Twitter gem & API calls are working.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@ class HomeController < ApplicationController
 
   def index
 
+    client = twitter_api_object
+
+    @elia = client.user_timeline("elia_mg")
+
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   # provider :github, YOUR_CLIENT_ID, YOUR_CLIENT_SECRET
   # provider :instagram, YOUR_CLIENT_ID, YOUR_CLIENT_SECRET
+  # provider :twitter, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"]
 end
