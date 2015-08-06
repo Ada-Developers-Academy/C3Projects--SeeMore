17cf7071d4e32836c3a5fa380b8dcc62bc17da34
Anita <asovay@gmail.com>
2015-08-05 18:09:19 -0700

    added ability to access twitter API via twitter gem, class Twit

--- app/controllers/twitters_controller.rb
+++ app/controllers/twitters_controller.rb
@@ -1,18 +1,14 @@
-require 'httparty'
-require 'twitter'
-load 'config/initializers/twitter.rb'
-
 class TwittersController < ApplicationController
 
   # search page: search for twitter user
   # def search; end
 
-  TWITTER_URI = "https://api.twitter.com/1.1"
+  # TWITTER_URI = "https://api.twitter.com/1.1"
 
-  SEARCH_URI = "/users/search.json?"
+  # SEARCH_URI = "/users/search.json?"
   # "q=#{username}"
 
-  FEED_URI = "/statuses/user_timeline.json?"
+  # FEED_URI = "/statuses/user_timeline.json?"
   # "user_id=#{provider_id}&screen_name=#{username}"
 
   # to refactor into application controller, accepting params
@@ -26,13 +22,9 @@ class TwittersController < ApplicationController
 
   def search
     @username = params[:username]
+    @response = @twitter.client.user_search("#{@username}")
 
     # response = HTTParty.get(TWITTER_URI + SEARCH_URI + "q=#{@username}")
-
-    response = twitter.user_search("#{@username}")
-
-    @response_hash = response
-
   end
 
 end
