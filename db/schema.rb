a36a7e87cfc8922b459ce1a29d77552dff514cc5
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-06 13:57:34 -0700

    Adds subscription migration to schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805185922) do
+ActiveRecord::Schema.define(version: 20150806202309) do
 
   create_table "followees", force: :cascade do |t|
     t.string   "handle"
@@ -20,6 +20,18 @@ ActiveRecord::Schema.define(version: 20150805185922) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "followee_id"
+    t.datetime "subscribe_date"
+    t.datetime "unsubscribe_date"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "subscriptions", ["followee_id"], name: "index_subscriptions_on_followee_id"
+  add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
