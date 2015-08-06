c8dd60e0a5c8d03c88a87bffa8481fc8d3909b5a
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-06 16:56:09 -0700

    Updates schema with new tweets foregin key column.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805211744) do
+ActiveRecord::Schema.define(version: 20150806234516) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "tw_user_id_str"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150805211744) do
     t.integer  "tw_favorite_count"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
