2f30dd4f2e0e2a89a3f3a4ffbc1bfb3ba506c8b3
MisShellyMac <michellemccarthy@hotmail.com>
2015-08-06 13:15:03 -0700

    Uncommented spec tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -121,62 +121,62 @@ RSpec.describe SessionsController, type: :controller do
     end
 
     context "when using instagram authentication" do
-    #   context "is successful" do
-    #     before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:instagram] }
-    #
-    #     it "redirects to the homepage" do
-    #       get :create, provider: :instagram
-    #
-    #       expect(response).to redirect_to root_path
-    #     end
-    #
-    #     it "creates a stalker" do
-    #        expect { get :create, provider: :instagram }.to change(Stalker, :count).by(1)
-    #     end
-    #
-    #     it "assigns session[:stalker_id]" do
-    #       get :create, provider: :instagram
-    #       expect(assigns(:stalker)).to be_an_instance_of Stalker
-    #     end
-    #   end
-    #
-    #   context "stalker already exists" do
-    #     before(:each) do
-    #       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:instagram]
-    #       get :create, provider: :instagram
-    #     end
-    #     # before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:instagram] }
-    #     let(:stalker) { Stalker.find_or_create_from_auth_hash(OmniAuth.config.mock_auth[:instagram]) }
-    #
-    #     it "doesn't create a new stalker" do
-    #       get :create, provider: :instagram
-    #
-    #       expect(Stalker.count).to eq 1
-    #     end
-    #
-    #     it "assigns session[:stalker_id]" do
-    #       get :create, provider: :instagram
-    #
-    #       expect(session[:stalker_id]).to eq stalker.id
-    #     end
-    #   end
-    #
-    #   context "when failing to save the stalker" do
-    #     let(:invalid_params) { {
-    #       "provider" => "instagram",
-    #       "uid" => "1234",
-    #       "info" => { "nickname" => nil}
-    #       }
-    #     }
-    #     before { request.env["omniauth.auth"] = invalid_params }
-    #
-    #    it "redirects to home with flash error" do
-    #      get :create, provider: :instagram
-    #
-    #      expect(response).to redirect_to root_path
-    #      expect(flash[:error]).to_not be nil
-    #    end
-    #   end
+      context "is successful" do
+        before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:instagram] }
+
+        it "redirects to the homepage" do
+          get :create, provider: :instagram
+
+          expect(response).to redirect_to root_path
+        end
+
+        it "creates a stalker" do
+           expect { get :create, provider: :instagram }.to change(Stalker, :count).by(1)
+        end
+
+        it "assigns session[:stalker_id]" do
+          get :create, provider: :instagram
+          expect(assigns(:stalker)).to be_an_instance_of Stalker
+        end
+      end
+
+      context "stalker already exists" do
+        before(:each) do
+          request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:instagram]
+          get :create, provider: :instagram
+        end
+        # before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:instagram] }
+        let(:stalker) { Stalker.find_or_create_from_auth_hash(OmniAuth.config.mock_auth[:instagram]) }
+
+        it "doesn't create a new stalker" do
+          get :create, provider: :instagram
+
+          expect(Stalker.count).to eq 1
+        end
+
+        it "assigns session[:stalker_id]" do
+          get :create, provider: :instagram
+
+          expect(session[:stalker_id]).to eq stalker.id
+        end
+      end
+
+      context "when failing to save the stalker" do
+        let(:invalid_params) { {
+          "provider" => "instagram",
+          "uid" => "1234",
+          "info" => { "nickname" => nil}
+          }
+        }
+        before { request.env["omniauth.auth"] = invalid_params }
+
+       it "redirects to home with flash error" do
+         get :create, provider: :instagram
+
+         expect(response).to redirect_to root_path
+         expect(flash[:error]).to_not be nil
+       end
+      end
     end
   end
 
