3e1ec5409c98910d70defef4ce42506896558fd0
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-06 16:54:58 -0700

    Ordered tweets of subscribed tweeters; and displaying on home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,20 @@ class HomeController < ApplicationController
   def index
 
     client = twitter_api_object
+    twitter_ids = User.find(session[:user_id]).twi_subscriptions.pluck(:twitter_id)
 
-    @elia = client.user_timeline("elia_mg")
-    @methods = client.methods
+    @sub_array = []
+
+    twitter_ids.each do |twitter_id|
+      client.user_timeline(twitter_id.to_i).each do |tweet|
+        @sub_array << tweet
+      end
+    end
+
+
+    @sub_array.sort! { |a,b| b.as_json["created_at"].to_time <=> a.as_json["created_at"].to_time }
+
+    # sub_array.order by time
   end
 
 end
