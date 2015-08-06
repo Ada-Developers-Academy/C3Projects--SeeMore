64c09673012ca5037e0b835f0054642f1930138a
Brenna <brennaleker@gmail.com>
2015-08-06 15:54:30 -0700

    added post information to database record

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -33,12 +33,30 @@ class InstagramController < ApplicationController
     unless feed
       feed_url = FEED_URI_A + params[:feed_id] + FEED_URI_B
       results  = HTTParty.get(feed_url)
-      results  = results["data"].first["caption"]["from"]
-      avatar   = results["profile_picture"]
-      name     = results["username"]
+      raise
+      feed_info  = results["data"].first["caption"]["from"]
+      avatar   = feed_info["profile_picture"]
+      name     = feed_info["username"]
       platform = "Instagram"
       platform_feed_id = params[:feed_id]
-      feed = Feed.create(name: name, avatar: avatar, platform: platform, platform_feed_id: platform_feed_id)
+      feed = Feed.create(
+        name: name,
+        avatar: avatar,
+        platform: platform,
+        platform_feed_id: platform_feed_id
+      )
+      posts = results["data"]
+      posts.each do |post|
+        description = post["caption"]["text"]
+        content = post["images"]["low_resolution"]["url"]
+        date_posted = Time.at(post["caption"]["created_time"].to_i)
+        Post.create(
+          description: description,
+          content: content,
+          date_posted: date_posted,
+          feed_id: feed.id
+        )
+      end
     end
     current_user.feeds << feed unless current_user.feeds.include?(feed)
   end
