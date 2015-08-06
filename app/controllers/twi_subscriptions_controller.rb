382b4c0ec8d2bf227f5e799160bd2e7dc9e2c50e
Elia <elgrenier@gmail.com>
2015-08-06 14:52:22 -0700

    wip: twitter search form

--- app/controllers/twi_subscriptions_controller.rb
+++ app/controllers/twi_subscriptions_controller.rb
@@ -1,2 +1,11 @@
 class TwiSubscriptionsController < ApplicationController
+
+  def search
+  client =  twitter_api_object
+  client.user_search(params[:])
+  
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   delete '/logout', to: "sessions#destroy", as: "logout"
 
+  resources :twi_subscriptions
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
