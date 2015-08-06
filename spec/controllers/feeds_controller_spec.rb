073c464775907d6fa5ad87d2ab3d7dadcd3d1cf8
Loraine <loraine@lorainekv.com>
2015-08-05 17:58:51 -0700

    Added spec for search_results method.

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -2,28 +2,35 @@ require 'rails_helper'
 
 RSpec.describe FeedsController, type: :controller do
 
-describe "GET feeds#search" do
-  it "loads the search form" do
-    get :search
-    expect(response).to render_template(:search)
+  describe "GET feeds#search" do
+    it "loads the search form" do
+      get :search
+      expect(response).to render_template(:search)
+    end
   end
-end
 
-describe "POST feeds#search_redirect" do
-  context "if search term is present" do
-    it "redirects to the results page" do
+  describe "POST feeds#search_redirect" do
+    context "if search term is present" do
+      it "redirects to the results page" do
+        search_term = "donald trump"
+        post :search_redirect, search_term: search_term
+        expect(response).to redirect_to(search_results_path(search_term))
+      end
+    end
+
+    context "if search term is not present" do
+      it "redirects to the search form" do
+        post :search_redirect
+        expect(response).to redirect_to(search_path)
+      end
+    end
+  end
+
+  describe "GET feeds#search_results" do
+    it "receives a JSON response" do
       search_term = "donald trump"
-      post :search_redirect, search_term: search_term
-      expect(response).to redirect_to(search_results_path(search_term))
+      get :search_results, search_term: search_term
+      expect(@result.class).to be(JSON)
     end
   end
-
-  context "if search term is not present" do
-    it "redirects to the search form" do
-      post :search_redirect
-      expect(response).to redirect_to(search_path)
-    end
-  end
-end
-
 end
