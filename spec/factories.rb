eaf37298ff99df468f5b57d6d9b0d507a1619e3a
Loraine <loraine@lorainekv.com>
2015-08-06 15:16:12 -0700

    Created a factory for tweets.

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,7 +6,16 @@ FactoryGirl.define do
   end
 
   factory :tweet do
-    
+    tw_user_id_str "100"
+    tw_user_name_str "Beyonce"
+    tw_user_profile_image_url "http://fakeurl.org/beyonce.jpg"
+    tw_user_screen_name "beyonce"
+    tw_id_str "500"
+    tw_text "Don't worry, Be Yonce"
+    tw_created_at "2015-08-06 15:07:59 -0700"
+    tw_retweet_count 5
+    tw_favorite_count 2
+    user_id 1
   end
 
 end
