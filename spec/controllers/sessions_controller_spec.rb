2cf201f7f6edc370be4e489eae300dd6ee987c3a
Katie Atrops <katrops@gmail.com>
2015-08-06 14:32:45 -0700

    WIP session controller specs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -14,12 +14,14 @@ RSpec.describe SessionsController, type: :controller do
   describe "GET #destroy" do
     it "returns http success" do
       delete :destroy
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "#create_vimeo" do
+    let(:user) { AuUser.find_or_create_by(OmniAuth.config.mock_auth[:vimeo])}
     it "returns http success" do
+      session[:user_id] = user.id
       get :create_vimeo
       expect(response).to have_http_status(:success)
     end
