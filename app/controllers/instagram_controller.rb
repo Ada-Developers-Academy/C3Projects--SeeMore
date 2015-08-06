511a352cf17d516ed88ff7863ed868abb538314b
Brenna <brennaleker@gmail.com>
2015-08-06 16:13:03 -0700

    post data is persisting to db

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -31,32 +31,7 @@ class InstagramController < ApplicationController
   def subscribe
     feed = Feed.find_by(platform_feed_id: params[:feed_id])
     unless feed
-      feed_url = FEED_URI_A + params[:feed_id] + FEED_URI_B
-      results  = HTTParty.get(feed_url)
-      raise
-      feed_info  = results["data"].first["caption"]["from"]
-      avatar   = feed_info["profile_picture"]
-      name     = feed_info["username"]
-      platform = "Instagram"
-      platform_feed_id = params[:feed_id]
-      feed = Feed.create(
-        name: name,
-        avatar: avatar,
-        platform: platform,
-        platform_feed_id: platform_feed_id
-      )
-      posts = results["data"]
-      posts.each do |post|
-        description = post["caption"]["text"]
-        content = post["images"]["low_resolution"]["url"]
-        date_posted = Time.at(post["caption"]["created_time"].to_i)
-        Post.create(
-          description: description,
-          content: content,
-          date_posted: date_posted,
-          feed_id: feed.id
-        )
-      end
+      save_feed
     end
     current_user.feeds << feed unless current_user.feeds.include?(feed)
   end
