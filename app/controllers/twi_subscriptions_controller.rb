3c721c2cabfce9d74cb3e4e26e79d2a3648675ca
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-06 15:47:38 -0700

    Successfully have subscriptions working\!\! YAY

--- app/controllers/twi_subscriptions_controller.rb
+++ app/controllers/twi_subscriptions_controller.rb
@@ -5,6 +5,19 @@ class TwiSubscriptionsController < ApplicationController
     @results = client.user_search(params[:twitter_search])
   end
 
+  def create
+    subscription = TwiSubscription.create(twitter_id: params[:twitter_id])
 
+    user = User.find(session[:user_id])
+
+    user.twi_subscriptions << subscription
+    user.save
+
+    redirect_to root_path
+  end
+
+  # def twitter_sub_params
+  #   params.require(:)
+  # end
 
 end
