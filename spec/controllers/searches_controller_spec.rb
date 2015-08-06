249c826a1173fb9c2a6c2a370e402a510d8ea92e
Shanna Chau <shannachau@gmail.com>
2015-08-06 16:24:53 -0700

    Added tests for POST #search for Searches Controller

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe SearchesController, type: :controller do
+  let (:invalid_search) { "" }
+  let (:valid_search)   { "words" }
 
+  describe "POST #search" do
+    context "searching for a Twitter user" do
+      it "redirects you to home page when you don't enter a search term" do
+        post :search, search_twitter: invalid_search
+        expect(response).to redirect_to(root_path)
+      end
+
+      it "redirects you to the search show page when you enter a search term" do
+        post :search, search_twitter: valid_search
+        expect(response).to redirect_to(search_results_path("twitter", valid_search))
+      end
+    end
+
+    pending "searching for an Instagram user"
+  end
+
+  describe "GET #index" do
+    context "searching for a Twitter user" do
+
+    end
+  end
 end
