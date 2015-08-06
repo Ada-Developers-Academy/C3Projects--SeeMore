2aabf6a1e657c54577d8caf9425f5c2faa7bd098
Corinne Pingul <corinnepingul@gmail.com>
2015-08-06 08:01:31 -0700

    Adds controller tests for vimeo.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,7 +3,18 @@ require 'rails_helper'
 RSpec.describe SessionsController, type: :controller do
   describe "GET #create" do
     context "when using developer strategies" do
+      # before { request.env } what goes here???
+      it "redirects to the homepage" do
 
+      end
+
+      it "doesn't duplicate a user" do
+
+      end
+
+      it "sets session[:user_id]" do
+
+      end
     end
 
     context "when using twitter authentication" do
@@ -66,11 +77,121 @@ RSpec.describe SessionsController, type: :controller do
     end
 
     context "when using vimeo authentication" do
+      context "is successful" do
+        before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:vimeo] }
 
+        it "redirects to the homepage" do
+          get :create, provider: :vimeo
+
+          expect(response).to redirect_to root_path
+        end
+
+        it "creates a user" do
+           expect { get :create, provider: :vimeo }.to change(Stalker, :count).by(1)
+        end
+
+        it "assigns session[:user_id]" do
+          get :create, provider: :vimeo
+          expect(assigns(:user)).to be_an_instance_of Stalker
+        end
+      end
+
+      context "user already exists" do
+        before(:each) do
+          request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:vimeo]
+          get :create, provider: :vimeo
+        end
+        # before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:vimeo] }
+        let(:user) { Stalker.find_or_create_from_auth_hash(OmniAuth.config.mock_auth[:vimeo]) }
+
+        it "doesn't create a new user" do
+          get :create, provider: :vimeo
+
+          expect(Stalker.count).to eq 1
+        end
+
+        it "assigns session[:user_id]" do
+          get :create, provider: :vimeo
+
+          expect(session[:user_id]).to eq user.id
+        end
+      end
+
+      context "when failing to save the user" do
+        let(:invalid_params) { {
+          "provider" => "vimeo",
+          "uid" => "1234",
+          "info" => { "name" => nil}
+          }
+        }
+        before { request.env["omniauth.auth"] = invalid_params }
+
+       it "redirects to home with flash error" do
+         get :create, provider: :vimeo
+
+         expect(response).to redirect_to root_path
+         expect(flash[:error]).to_not be nil
+       end
+      end
     end
 
     context "when using instagram authentication" do
-
+    #   context "is successful" do
+    #     before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:instagram] }
+    #
+    #     it "redirects to the homepage" do
+    #       get :create, provider: :instagram
+    #
+    #       expect(response).to redirect_to root_path
+    #     end
+    #
+    #     it "creates a user" do
+    #        expect { get :create, provider: :instagram }.to change(Stalker, :count).by(1)
+    #     end
+    #
+    #     it "assigns session[:user_id]" do
+    #       get :create, provider: :instagram
+    #       expect(assigns(:user)).to be_an_instance_of Stalker
+    #     end
+    #   end
+    #
+    #   context "user already exists" do
+    #     before(:each) do
+    #       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:instagram]
+    #       get :create, provider: :instagram
+    #     end
+    #     # before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:instagram] }
+    #     let(:user) { Stalker.find_or_create_from_auth_hash(OmniAuth.config.mock_auth[:instagram]) }
+    #
+    #     it "doesn't create a new user" do
+    #       get :create, provider: :instagram
+    #
+    #       expect(Stalker.count).to eq 1
+    #     end
+    #
+    #     it "assigns session[:user_id]" do
+    #       get :create, provider: :instagram
+    #
+    #       expect(session[:user_id]).to eq user.id
+    #     end
+    #   end
+    #
+    #   context "when failing to save the user" do
+    #     let(:invalid_params) { {
+    #       "provider" => "instagram",
+    #       "uid" => "1234",
+    #       "info" => { "nickname" => nil}
+    #       }
+    #     }
+    #     before { request.env["omniauth.auth"] = invalid_params }
+    #
+    #    it "redirects to home with flash error" do
+    #      get :create, provider: :instagram
+    #
+    #      expect(response).to redirect_to root_path
+    #      expect(flash[:error]).to_not be nil
+    #    end
+    #   end
     end
   end
 
