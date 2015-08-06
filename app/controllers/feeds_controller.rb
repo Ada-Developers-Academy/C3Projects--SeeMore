2fd291564b25831495caef5ff802781d9d2c5b0c
Loraine <loraine@lorainekv.com>
2015-08-05 17:59:29 -0700

    WIP search results.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,19 +1,20 @@
 class FeedsController < ApplicationController
 
+  TWITTER_URI = "https://api.twitter.com/1.1/users/search.json?q="
+
   def search; end
 
   def search_redirect
     if params[:search_term].present?
-      redirect_to search_results_path
+      redirect_to search_results_path(params[:search_term])
     else
       redirect_to search_path
     end
   end
 
   def search_results
-
-    #Actual stuff
-
+    @search_term = params[:search_term]
+    @results = HTTParty.get(TWITTER_URI + "#{@search_term}")
   end
 
 end
