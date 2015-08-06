5fc33f3579cda433208288bf03d5d43a0151e5a9
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-05 20:11:11 -0700

    added test for #landing_page

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -16,12 +16,37 @@ RSpec.describe HomeController, type: :controller do
     end
   end
 
-  describe "POST #twitter_users_redirect" do
+  describe "GET #landing_page" do
+    let(:user) { User.create(
+      uid: "kate@plate.com", provider: "developer", name: "Kate", email: "kate@plate.com"
+      ) }
 
+    before :each do
+      session[:user_id] = user.id
+      get :landing_page
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    # it "finds the current user" do
+    #   binding.pry
+    #   expect(@current_user).to eq user
+    # end
+
+    # it "doesn't find the current user if none set" do
+    #   session[:user_id] = nil
+    #   expect(@current_user).to be_nil
+    # end
+  end
+
+  describe "POST #twitter_users_redirect" do
     context "search term entered" do
       before :each do
         post :twitter_users_redirect, user: "pig"
       end
+
       it "redirects to twitter_users_path if params[:user]" do
         # expect(response).to have_http_status(302)
         expect(response).to redirect_to(twitter_users_path("pig"))
@@ -33,10 +58,18 @@ RSpec.describe HomeController, type: :controller do
       end
     end
 
-    it "redirects to search_path if no search term entered" do
-      post :twitter_users_redirect, user: nil
-      expect(response).to redirect_to(search_path)
+    context "no search term entered" do
+      it "redirects to search_path" do
+        post :twitter_users_redirect, user: nil
+        expect(response).to redirect_to(search_path)
+      end
     end
   end
 
+  describe "GET #twitter_users" do
+    it "renders the twitter users template" do
+      get :twitter_users, user: "cow"
+      expect(response).to render_template("twitter_users")
+    end
+  end
 end
