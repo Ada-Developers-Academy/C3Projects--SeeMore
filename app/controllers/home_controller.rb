7b011f42cacae6ad860eeb1d5b41c8b4ca671cbc
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-05 20:10:31 -0700

    fixed bug in @current_user assignment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def landing_page
-    @current_user = User.find(session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def search
