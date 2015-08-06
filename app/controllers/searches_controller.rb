5036ba41a16323fd97af5b37a26b4da03e70b47a
Shanna Chau <shannachau@gmail.com>
2015-08-06 13:03:14 -0700

    Added form for instagram search as a placeholder. Also added comments for the scaffolding to implement the instagram search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,11 +9,22 @@ class SearchesController < ApplicationController
         redirect_to search_results_path("twitter", params[:search_twitter])
       end
     end
+
+    # search via the instagram form
+    # if params[:search_instagram]
+    #   if params[:search_instagram] == ""
+    #     redirect_to root_path
+    #   else
+    #     redirect_to search_results_path("instagram", params[:search_instagram])
+    #   end
+    # end
   end
 
   def show
     if params[:client] == "twitter"
       @search_results = @twitter_client.client.user_search(params[:search_term])
+    elsif params[:client] == "instagram"
+      # put code for instagram search results here
     end
   end
 end
