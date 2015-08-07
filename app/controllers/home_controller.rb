4d193a46d55009bb6556ca3fbf38a28edbaf58c1
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-06 17:18:58 -0700

    Continue setting up and sorting tweets.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,21 +2,24 @@ class HomeController < ApplicationController
 
   def index
 
-    client = twitter_api_object
-    twitter_ids = User.find(session[:user_id]).twi_subscriptions.pluck(:twitter_id)
+    unless session[:user_id].nil?
+      client = twitter_api_object
+      twitter_ids = User.find(session[:user_id]).twi_subscriptions.pluck(:twitter_id)
 
-    @sub_array = []
+      @sub_array_tweets = []
 
-    twitter_ids.each do |twitter_id|
-      client.user_timeline(twitter_id.to_i).each do |tweet|
-        @sub_array << tweet
+      twitter_ids.each do |twitter_id|
+        client.user_timeline(twitter_id.to_i).each do |tweet|
+          @sub_array_tweets << tweet
+        end
       end
+
+      @sub_array_tweets.sort! { |a,b| b.as_json["created_at"].to_time <=> a.as_json["created_at"].to_time }
+
+      # could transform tweets into our own "post" object...
+      # need to combine
     end
 
-
-    @sub_array.sort! { |a,b| b.as_json["created_at"].to_time <=> a.as_json["created_at"].to_time }
-
-    # sub_array.order by time
   end
 
 end
