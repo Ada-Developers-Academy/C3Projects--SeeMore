b73397cc4684e5eddb2eb0b9369c00d0dbf87a42
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-07 10:15:52 -0700

    Edited and added user model specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,13 +16,6 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
 
-    it "requires a name" do
-      user.name = nil
-
-      expect(user).to be_invalid
-      expect(user.errors.keys).to include(:name)
-    end
-
     it "requires a uid" do
       user.uid = nil
 
@@ -37,15 +30,6 @@ RSpec.describe User, type: :model do
       expect(user.errors.keys).to include(:provider)
     end
 
-    it "name has to be unique" do
-      user.save
-
-      user2 = User.create(name: "Ada")
-
-      expect(user2).to be_invalid
-      expect(user2.errors.keys).to include(:name)
-    end
-
     it "uid has to be unique" do
       user.save
 
@@ -57,49 +41,75 @@ RSpec.describe User, type: :model do
   end
 
   describe ".initialize_from_omniauth" do
-    let(:user) { User.find_or_create_user(OmniAuth.config.mock_auth[:developer]) }
+    context "developer auth" do
+      let(:user) { User.find_or_create_user(OmniAuth.config.mock_auth[:developer]) }
 
-    it "creates a valid user" do
-      expect(user).to be_valid
-    end
+      it "creates a valid user" do
+        expect(user).to be_valid
+      end
 
-    context "when it's invalid" do
-      it "returns nil" do
-        user = User.find_or_create_user({"uid" => "123", "info" => {}})
-        expect(user).to be_nil
+      context "when it's invalid" do
+        it "returns nil" do
+          user = User.find_or_create_user({"uid" => "123", "info" => {}})
+          expect(user).to be_nil
+        end
       end
     end
 
-    let(:user) { User.find_or_create_user(OmniAuth.config.mock_auth[:instagram]) }
+    context "instagram auth" do
+      let(:user) { User.find_or_create_user(OmniAuth.config.mock_auth[:instagram]) }
 
-    it "creates a valid user" do
-      expect(user).to be_valid
-    end
+      it "creates a valid user" do
+        expect(user).to be_valid
+      end
 
-    context "when it's invalid" do
-      it "returns nil" do
-        user = User.find_or_create_user({"uid" => "123", "info" => {}})
-        expect(user).to be_nil
+      context "when it's invalid" do
+        it "returns nil" do
+          user = User.find_or_create_user({"uid" => "123", "info" => {}})
+          expect(user).to be_nil
+        end
       end
     end
   end
 
+  describe "#associate_subscription" do
+    it "when called on a user object associates the provided twitter subscription" do
+      user.save
+      twisub = TwiSubscription.create(twitter_id: "@beast")
+
+      user.associate_subscription(twisub)
+
+      expect(user.twi_subscriptions.count).to eq 1
+      expect(user.twi_subscriptions).to include(twisub)
+    end
+
+    # it "when called on a user object associates the provided instagram subscription" do
+    #   user.save
+    #   igsub = IgSubscription.create(instagram_id: "@beast")
+    #
+    #   user.associate_subscription(igsub)
+    #
+    #   expect(user.ig_subscriptions.count).to eq 1
+    #   expect(user.ig_subscriptions).to include(igsub)
+    # end
+  end
+
   describe "model associations" do
     it "a user has and belongs to an IG subscription" do
-      user = User.create(name: "user1", provider: "developer", uid: "uid1")
+      user.save
       igsub = IgSubscription.create(instagram_id: "@beast")
       igsub.users << user
 
-      expect(igsub.users.first.name).to eq "user1"
+      expect(igsub.users.first.uid).to eq "12354"
       expect(user.ig_subscriptions.first.instagram_id).to eq "@beast"
     end
 
     it "a user has and belongs to an Twitter subscription" do
-      user = User.create(name: "user1", provider: "developer", uid: "uid1")
+      user.save
       twisub = TwiSubscription.create(twitter_id: "@beast")
       twisub.users << user
 
-      expect(twisub.users.first.name).to eq "user1"
+      expect(twisub.users.first.uid).to eq "12354"
       expect(user.twi_subscriptions.first.twitter_id).to eq "@beast"
     end
   end
