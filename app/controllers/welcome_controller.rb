c808f1e7e13d61f2ef1ad6b7742125a17d7bee63
Jeri <drvonnjerryxlii@gmail.com>
2015-08-07 09:39:30 -0700

    updated welcome#index to use less chaining & added a couple notes to post model for our future selves

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,8 @@ class WelcomeController < ApplicationController
   def index
     # reset_session # TODO: remove this before final PR. keep until no more rake db:reset
     if current_user
-      @posts = current_user.posts.chronological.only_thirty
+      posts = current_user.posts
+      @posts = posts.only_thirty
     end
   end
 end
