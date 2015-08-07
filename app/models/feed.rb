3a0f87ead434c52c859f320c224f7cb40f7566b1
Victoria <vikshab2010@hotmail.com>
2015-08-06 20:11:00 -0700

    Added validations to Post and Feed models

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,4 +2,7 @@ class Feed < ActiveRecord::Base
   # Associations--------------------------------------------
   has_and_belongs_to_many :au_users
   has_many :posts
+
+  # Validations---------------------------------------------
+  validates :name, :platform, :platform_feed_id, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
   # Associations--------------------------------------------
   belongs_to :feed
+
+  # Validations---------------------------------------------
+  validates :description, :content, :date_posted, :feed_id, presence: true
 end
