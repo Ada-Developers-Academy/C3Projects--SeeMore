8b2a0ab6abd8918ee3be95a366a49a5e0937b3ea
Elia <elgrenier@gmail.com>
2015-08-06 17:48:22 -0700

    wip: extracting methods into model methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :assign_user
 
   private
+
+    def assign_user
+      unless session[:user_id].nil?
+        @user = User.find(session[:user_id])
+      end
+    end
 
     def twitter_api_object
       Twitter::REST::Client.new do |config|
