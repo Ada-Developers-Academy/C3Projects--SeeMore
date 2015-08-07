51ab5c45416251671238bc0844aab103484ac86a
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-06 17:12:12 -0700

    added native_id attribute to followee model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806223126) do
+ActiveRecord::Schema.define(version: 20150807000808) do
 
   create_table "followees", force: :cascade do |t|
     t.string   "handle"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150806223126) do
     t.string   "avatar_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "native_id"
   end
 
   create_table "posts", force: :cascade do |t|
