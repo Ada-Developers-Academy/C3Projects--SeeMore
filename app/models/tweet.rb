11609e8cfc4ac802953ce9e29b263705ea1a0a00
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-07 14:22:32 -0700

    Fixed crazy spacing.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -10,17 +10,18 @@ class Tweet < ActiveRecord::Base
     # look up tweets for that twitter user
     tweets = @twit_init.client.user_timeline(tw_user).take(5)
     # for each tweet make the foreign key user_id = the sessions user_id
-    tweets.each do |tweet| our_tweet = Tweet.new
-                          our_tweet.tw_user_id_str = tweet.user.id
-                          our_tweet.tw_user_name_str = tweet.user.name
-                          our_tweet.tw_user_image_url = tweet.user.profile_image_url
-                          our_tweet.tw_user_screen_name = tweet.user.screen_name
-                          our_tweet.tw_id_str = tweet.id_str
-                          our_tweet.tw_text = tweet.text
-                          our_tweet.tw_created_at = tweet.created_at
-                          our_tweet.tw_retweet_count = tweet.retweet_count
-                          our_tweet.tw_favorite_count = tweet.favorite_count
-                          our_tweet.user_id = session[:user_id]
+    tweets.each do |tweet|
+      our_tweet = Tweet.new
+      our_tweet.tw_user_id_str = tweet.user.id
+      our_tweet.tw_user_name_str = tweet.user.name
+      our_tweet.tw_user_image_url = tweet.user.profile_image_url
+      our_tweet.tw_user_screen_name = tweet.user.screen_name
+      our_tweet.tw_id_str = tweet.id_str
+      our_tweet.tw_text = tweet.text
+      our_tweet.tw_created_at = tweet.created_at
+      our_tweet.tw_retweet_count = tweet.retweet_count
+      our_tweet.tw_favorite_count = tweet.favorite_count
+      our_tweet.user_id = session[:user_id]
       # save the tweets in the db
       our_tweet.save
     end
