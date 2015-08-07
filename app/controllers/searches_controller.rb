a7b4a151b3e648ef355bab314d41041d5e67195c
Corinne Pingul <corinnepingul@gmail.com>
2015-08-07 09:48:07 -0700

    Implements searching instagram users.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,6 @@
 class SearchesController < ApplicationController
+  INSTAGRAM_SEARCH_USERS_URI = "https://api.instagram.com/v1/users/search?client_id=#{ENV["INSTAGRAM_CLIENT_ID"]}&"
+
   def index
     if params[:search_twitter]
       if params[:search_twitter] == ""
@@ -10,20 +12,21 @@ class SearchesController < ApplicationController
 
     # OPTIMIZE: refactor so there are less nested if's
     # search via the instagram form
-    # if params[:search_instagram]
-    #   if params[:search_instagram] == ""
-    #     redirect_to root_path
-    #   else
-    #     redirect_to search_results_path("instagram", params[:search_instagram])
-    #   end
-    # end
+    if params[:search_instagram]
+      if params[:search_instagram] == ""
+        redirect_to root_path
+      else
+        redirect_to search_results_path("instagram", params[:search_instagram])
+      end
+    end
   end
 
   def show
     if params[:client] == "twitter"
       @search_results = TwitterClient.user_search(params[:search_term])
     elsif params[:client] == "instagram"
-      # TODO: put code to search instagram users here
+      @search_results = HTTParty.get(
+                        INSTAGRAM_SEARCH_USERS_URI + "q=#{params[:search_term]}")
     end
   end
 end
