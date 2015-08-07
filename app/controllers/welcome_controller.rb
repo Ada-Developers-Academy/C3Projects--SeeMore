3a874ec281042a340933af6f13c6977a4c51e9e5
Jeri <drvonnjerryxlii@gmail.com>
2015-08-06 21:36:33 -0700

    adjusted welcome#home & related views to display subscribed posts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,8 @@
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    # reset_session # TODO: remove this before final PR. keep until no more rake db:reset
+    if current_user
+      @posts = current_user.posts.chronological.only_thirty
+    end
+  end
 end
