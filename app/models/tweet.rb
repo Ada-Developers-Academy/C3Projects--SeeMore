f34c6ca187dcd8a2b8c43d2a2f717bf20006f61a
Loraine <loraine@lorainekv.com>
2015-08-07 15:10:13 -0700

    Wrote a following? check method.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -26,4 +26,23 @@ class Tweet < ActiveRecord::Base
       our_tweet.save
     end
   end
+
+  def following(id)?
+    # This checks to see if tweets by this twitter user exist
+    matches = Tweet.find_by(tw_user_id_str: id)
+    # Checks if there are any matches
+    unless matches == nil
+    # This checks if that collection of matches are associated with
+    # the Creep Peep user
+    matches.first.user_id == session[:user_id]
+      # This conditional might not be necessary...
+      # if matches.first.user_id == session[:user_id]
+      #   return true
+      # else
+      #   return false
+      # end
+    else
+      return false
+    end
+  end
 end
