3aa019c15df391143bf6b00bddad25a1c517f22f
Elia <elgrenier@gmail.com>
2015-08-06 18:10:06 -0700

    omniauth access token config

--- app/models/twi_subscription.rb
+++ app/models/twi_subscription.rb
@@ -5,4 +5,16 @@ class TwiSubscription < ActiveRecord::Base
   # Associations
   has_and_belongs_to_many :users
 
+  #Scopes
+  # scope :find_twitter_id, -> (twitter_id) { find_by(twitter_id: twitter_id) }
+
+  # def self.find_or_create_subscription(twitter_id)
+  #   subscription = find_twitter_id(twitter_id)
+  #
+  #   if subscription.nil?
+  #     return TwiSubscription.create(twitter_id: twitter_id)
+  #   else
+  #     return subscription
+  #   end
+  # end
 end
