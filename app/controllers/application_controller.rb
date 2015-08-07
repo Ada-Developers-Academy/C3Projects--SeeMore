d7bcec7cec11dbd21dc758b257503a3919cd6e23
Anita <asovay@gmail.com>
2015-08-07 10:22:43 -0700

    fixed error flashing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
 
   def require_login
     unless session[:user_id]
-      redirect_to root_path, flash: { errors: MESSAGES[:login_required] }
+      redirect_to root_path, flash: { error: MESSAGES[:login_required] }
     end
   end
 end
