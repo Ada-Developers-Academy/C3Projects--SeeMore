2fa81082be99a6780ed5b3a9144d3678740807a5
Loren Wang <wang131.131@gmail.com>
2015-08-07 10:48:37 -0700

    instagram followers working. feed wip

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,6 +2,7 @@ class FeedsController < ApplicationController
 
   def index
     @user = User.find_by(id: session[:user_id])
+    @people = Instagram.find(@user.instagram_ids)
   end
 
   def search; end
