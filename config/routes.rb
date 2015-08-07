80ee970525f36c3973c47e8063fe9b22835d9ab7
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-07 10:35:04 -0700

    consolidated search routes to use single page

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,16 @@ Rails.application.routes.draw do
 
 
   # Search page
-  get '/insta_search', to: "followees#insta_search", as: "insta_search"
-  get '/search', to: 'home#search', as: 'search'
+  # get '/insta_search', to: "followees#insta_search", as: "insta_search"
+  # get '/search', to: 'home#search', as: 'search'
+  get '/search_final', to: 'followees#search', as: "search_final"
   
   # Redirect (checks if search field is filled in)
   post "/instagram_users", to: "followees#instagram_users_redirect", as: "instagram_users_redirect"
   post '/twitter_users', to: 'followees#twitter_users_redirect', as: 'twitter_users_redirect'
 
   # Search Results
-  get '/search/:user', to: "followees#search_results", as: "search_results"
+  get '/search/:source/:user', to: "followees#search_results", as: "search_results"
   # get "/instagram/:user" => "followees#insta_users", as: "iusers"
   # get '/twitter_users/:user', to: 'home#twitter_users', as: 'twitter_users'
 
@@ -26,6 +27,5 @@ Rails.application.routes.draw do
   # this route is for de-bugging fetching a user's posts. Prob will delete later
   get '/instagram/:user/posts', to: 'followees#insta_user_posts', as: "iuser_posts"
 
-  get '/search_final', to: 'followees#search', as: "search_final"
 
 end
