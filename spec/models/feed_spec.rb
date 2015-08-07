640ab4b02166e93406f3a5e6837865e0eb523e43
Victoria <vikshab2010@hotmail.com>
2015-08-06 20:58:31 -0700

    Changed spec_helper: OmniAuth.config.mock_auth Model validations test for Feed and Post models - all green

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Feed, type: :model do
-  before :each do
-    @feed = create :feed
-  end
+  describe "validations" do
+    before :each do
+      @feed = create :feed
+    end
 
-  it "creates a new feed" do
-    expect(@feed).to be_valid
+    it "is valid" do
+      expect(@feed).to be_valid
+    end
+
+    it "requires a description" do
+      @feed.name = nil
+      expect(@feed).to be_invalid
+    end
+
+    it "requires a content" do
+      @feed.platform = nil
+      expect(@feed).to be_invalid
+    end
+
+    it "requires a date_posted" do
+      @feed.platform_feed_id = nil
+      expect(@feed).to be_invalid
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,11 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  before :each do
-    @post = create :post
+  describe "validations" do
+    before :each do
+      @post = create :post
+    end
+
+    it "is valid" do
+      expect(@post).to be_valid
+    end
+
+    it "requires a description" do
+      @post.description = nil
+      expect(@post).to be_invalid
+    end
+
+    it "requires a content" do
+      @post.content = nil
+      expect(@post).to be_invalid
+    end
+
+    it "requires a date_posted" do
+      @post.date_posted = nil
+      expect(@post).to be_invalid
+    end
+
+    it "requires a feed_id" do
+      @post.feed_id = nil
+      expect(@post).to be_invalid
+    end
   end
 
-  it "creates a new post" do
-    expect(@post).to be_valid
-  end
+
+  # describe ".initialize_from_omniauth" do
+  #   let(:post) { Post.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:instagram]) }
+  #
+  #   it "creates a valid post" do
+  #     expect(post).to be_valid
+  #   end
+  #
+  #   context "when it's invalid" do
+  #     it "returns nil" do
+  #       post = Post.find_or_create_from_omniauth({"uid" => "123", "data" => {}})
+  #       expect(post).to be_nil
+  #     end
+  #   end
+  # end
 end
