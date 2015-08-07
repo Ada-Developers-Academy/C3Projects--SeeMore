7d45ce21e3908f89893b47d3bc79530202a5efac
Jeri <drvonnjerryxlii@gmail.com>
2015-08-06 21:45:35 -0700

    moved post creation method to feed model updated subscribe buttons in feed pages & other minor tweaks there

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,7 +1,16 @@
 class Feed < ActiveRecord::Base
-  # Associations--------------------------------------------
+  # Associations ---------------------------------------------------------------
   has_and_belongs_to_many :au_users
   has_many :posts
+  after_create :populate_posts
+
+  # Validations ----------------------------------------------------------------
+  validates_presence_of :platform, :platform_feed_id
+
+  # Scopes ---------------------------------------------------------------------
+  scope :developer, -> { where(platform: "Developer") }
+  scope :instagram, -> { where(platform: "Instagram") }
+  scope :vimeo, -> { where(platform: "Vimeo") }
 
   def check_for_updates
     # query the API
@@ -10,4 +19,34 @@ class Feed < ActiveRecord::Base
       # privacy-- people might change the privacy stuff
       # deletion-- people might just delete something
   end
+
+  # not sure where to put these
+  FEED_URI_A = "https://api.instagram.com/v1/users/" # then user_id (for us: feed_id)
+  FEED_URI_B = "/media/recent?client_id=#{ ENV["INSTAGRAM_CLIENT_ID"] }"
+
+  def populate_posts
+    if platform == "Instagram" || platform == "Developer"
+      feed_url = FEED_URI_A + platform_feed_id.to_s + FEED_URI_B
+      results = HTTParty.get(feed_url)
+      posts = results["data"]
+      posts.each do |post|
+        maybe_valid_post = Post.create(create_instagram_post(post, self.id))
+        # raise
+      end
+
+    elsif platform == "Vimeo"
+      # TODO: handling for populating vimeo posts here
+
+    end
+  end
+
+  private
+    def create_instagram_post(post_data, feed_id)
+      post_hash = {}
+      post_hash[:description] = post_data["caption"]["text"] if post_data["caption"]
+      post_hash[:content]     = post_data["images"]["low_resolution"]["url"]
+      post_hash[:date_posted] = Time.at(post_data["created_time"].to_i)
+      post_hash[:feed_id]     = feed_id
+      return post_hash
+    end
 end
