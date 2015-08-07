442ed3597c176fe7bf2672b573202af5fd5fabd3
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-08-07 11:22:45 -0700

    Added rspec tests to user & twi_subscription models.

--- app/models/twi_subscription.rb
+++ app/models/twi_subscription.rb
@@ -5,15 +5,20 @@ class TwiSubscription < ActiveRecord::Base
   # Associations
   has_and_belongs_to_many :users
 
-  #Scopes
-  scope :find_twitter_id, -> (twitter_id) { find_by(twitter_id: twitter_id) }
+  # NOTE TO SELF: This does not work as a scope BECAUSE
+  # if it finds NOTHING it returns EVERYTHING.
+  # This is by design to make scopes chainable.
+  # Thx scopes.
+  def self.find_twitter_id(id)
+    find_by(twitter_id: id)
+  end
 
   # Uses twitter_id passed in params from the link_to button from the search results.
   def self.find_or_create_subscription(twitter_id)
     subscription = find_twitter_id(twitter_id)
 
     # If there's not a subscription, will create one or return the subscription found.
-    if subscription.empty? # not nil!
+    if subscription.nil?
       return create(twitter_id: twitter_id)
     else
       return subscription
--- spec/models/twi_subscription_spec.rb
+++ spec/models/twi_subscription_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe TwiSubscription, type: :model do
+  let(:twi_subscription) {
+    TwiSubscription.new(twitter_id: "123465")
+  }
+
   describe "model validations" do
-    let(:twi_subscription) {
-      TwiSubscription.new(twitter_id: "@beast")
-      }
 
     it "requires an twitter id" do
 
@@ -20,7 +22,7 @@ RSpec.describe TwiSubscription, type: :model do
 
     it "wont create a duplicate subscription" do
       twi_subscription.save
-      dup_sub = TwiSubscription.new(twitter_id: "@beast")
+      dup_sub = TwiSubscription.new(twitter_id: "123465")
 
       expect(dup_sub).to be_invalid
       expect(dup_sub.errors.keys).to include(:twitter_id)
@@ -30,12 +32,48 @@ RSpec.describe TwiSubscription, type: :model do
   describe "model associations" do
     it "a Twitter subscription has and belongs to a user" do
       user = User.create(name: "user1", provider: "developer", uid: "uid1")
-      twisub = TwiSubscription.create(twitter_id: "@beast")
+      twisub = TwiSubscription.create(twitter_id: "123465")
       user.twi_subscriptions << twisub
 
-      expect(user.twi_subscriptions.first.twitter_id).to eq "@beast"
+      expect(user.twi_subscriptions.first.twitter_id).to eq "123465"
       expect(twisub.users.first.name).to eq "user1"
     end
   end
 
+  describe "model methods" do
+    context "#find_twitter_id" do
+      it "finds a twitter subscription according to twitter_id" do
+        twi_subscription.save
+
+        expect(TwiSubscription.find_twitter_id("123465")).to eq twi_subscription
+      end
+
+      it "returns an empty ActiveRecord object if twitter_id doesn't exist" do
+        subscription = TwiSubscription.find_twitter_id("0")
+
+        expect(subscription).to be_nil
+        expect(TwiSubscription.count).to eq 0
+      end
+    end
+
+    context "#find_or_create_subscription" do
+      let(:twisub) { TwiSubscription.find_or_create_subscription("123465") }
+      it "if subscription does not exist, it creates it" do
+        twisub
+
+        expect(TwiSubscription.count).to eq 1
+        expect(TwiSubscription.first.twitter_id).to eq "123465"
+      end
+
+      it "returns created subscription" do
+        expect(twisub).to eq TwiSubscription.first
+      end
+
+      it "if subscription does exist, it finds & returns it" do
+        twi_subscription.save
+
+        expect(twisub).to eq twi_subscription
+      end
+    end
+  end
 end
