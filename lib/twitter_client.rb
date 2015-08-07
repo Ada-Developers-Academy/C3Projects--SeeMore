d094f53d5341a5b243d5704df4f7981c4b620846
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-07 10:44:23 -0700

    Adds search for posts from twitter user, no test.

--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -15,7 +15,12 @@ class TwitterClient
   end
 
   def user_timeline(user)
-    client.user_timeline(user).take(2)
-    # limit this so don't go over
+    # followee = Followee.find_by(handle: user)
+    # last_tweet_id = Posts.find_by(followee_id: followee.id).last
+    # client.user_timeline(user, options = {since_id: last_tweet_id} ) # - use this one in real setting
+    client.user_timeline(user)
+    # default returns the 20 most recent tweets of the user
+    # set the since_id parameter to the greatest ID of all the Tweets your application has already processed
+    # can search by user_id or screen_name
   end
 end
