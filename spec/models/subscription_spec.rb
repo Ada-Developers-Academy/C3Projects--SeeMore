b14f1be98e37cc7f51f1948b9545fe34db3d0272
Kari <kari.lonczak@gmail.com>
2015-08-08 15:37:51 -0700

    Updated subscription model spec for testing the active subscription scope. Passing now.

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -21,20 +21,21 @@ RSpec.describe Subscription, type: :model do
     end # context
   end #describe
 
-  describe ":active scope" do
-    let(:sub)   { create :subscription }
-    let(:sub2)  { create :subscription }
-    let(:sub3)  { create :subscription }
-  
-    it "selects subscriptions that do not have an unsubscribe_date" do
-      sub.unsubscribe_date = nil
-      sub2.unsubscribe_date = "Sat, 02 Aug 2015 22:12:25 UTC +00:00"
-      sub3.unsubscribe_date = "Sat, 01 Aug 2015 22:12:25 UTC +00:00"
+  describe "scopes" do
 
-      expect(sub.unsubscribe_date).to eq nil
-      expect(sub3.unsubscribe_date).to eq "Sat, 01 Aug 2015 22:12:25 UTC +00:00"
-      expect(Subscription.count).to eq 3
-      expect(Subscription.active.count).to eq 1
+    context ":active scope" do
+      let(:sub)   { create :subscription }
+      let(:sub2)  { create :subscription, unsubscribe_date: "Sat, 02 Aug 2015 22:12:25 UTC +00:00" }
+      let(:sub3)  { create :subscription, unsubscribe_date: "Sat, 01 Aug 2015 22:12:25 UTC +00:00" }
+    
+      it "selects subscriptions that do not have an unsubscribe_date" do
+        sub.reload
+        sub2.reload
+        sub3.reload
+
+        expect(Subscription.count).to eq 3
+        expect(Subscription.active.count).to eq 1
+      end
     end
 
   end
