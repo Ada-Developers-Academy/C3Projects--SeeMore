3bd52c94825852bae332d67099dd6c917fdbf642
Kari <kari.lonczak@gmail.com>
2015-08-08 14:04:07 -0700

    Updated insta_user_posts view to display the embedded instagram posts of a certain user. Created a helper method in the followees controller to hit the oembed instagram API to find the embedded html to display. These will likely move into the home controller once we move the display into the newsfeed view.

--- app/controllers/followees_controller.rb
+++ app/controllers/followees_controller.rb
@@ -3,7 +3,11 @@ class FolloweesController < ApplicationController
   INSTA_USER_POSTS_URI = "https://api.instagram.com/v1/users/"
   TWIT_URI = "https://api.twitter.com/1.1/statuses/user_timeline.json?"
   INSTA_OEMBED_URI = "http://api.instagram.com/oembed?omitscript=false&url="
+  
   before_action :find, only: [:destroy]
+  helper_method :get_embedded_html_instagram
+
+  include ActionView::Helpers::OutputSafetyHelper
 
   def new
     @followee = Followee.new
@@ -61,26 +65,28 @@ class FolloweesController < ApplicationController
     response = HTTParty.get(INSTA_USER_POSTS_URI + @user_id + "/media/recent/?count=3&access_token=" + ENV["INSTAGRAM_ACCESS_TOKEN"])
 
     @insta_user_posts = response["data"]
-    @first_short_link = @insta_user_posts.first["link"]
-
-    @embedded_response = HTTParty.get(INSTA_OEMBED_URI + "#{@first_short_link}")
+    link = @insta_user_posts.first["link"]
   
-    @test_embedded_response = {
-      "provider_url"=>"https://instagram.com/", 
-      "media_id"=>"1037200603677204145_6", 
-      "author_name"=>"nicole", 
-      "height"=>nil, 
-      "provider_name"=>"Instagram", 
-      "title"=>"Dear Tahoe, \nYou are wonderful. \nLove, Nico", 
-      "html"=>"<blockquote class=\"instagram-media\" data-instgrm-captioned data-instgrm-version=\"4\" style=\" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);\"><div style=\"padding:8px;\"> <div style=\" background:#F8F8F8; line-height:0; margin-top:40px; padding:50% 0; text-align:center; width:100%;\"> <div style=\" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAAGFBMVEUiIiI9PT0eHh4gIB4hIBkcHBwcHBwcHBydr+JQAAAACHRSTlMABA4YHyQsM5jtaMwAAADfSURBVDjL7ZVBEgMhCAQBAf//42xcNbpAqakcM0ftUmFAAIBE81IqBJdS3lS6zs3bIpB9WED3YYXFPmHRfT8sgyrCP1x8uEUxLMzNWElFOYCV6mHWWwMzdPEKHlhLw7NWJqkHc4uIZphavDzA2JPzUDsBZziNae2S6owH8xPmX8G7zzgKEOPUoYHvGz1TBCxMkd3kwNVbU0gKHkx+iZILf77IofhrY1nYFnB/lQPb79drWOyJVa/DAvg9B/rLB4cC+Nqgdz/TvBbBnr6GBReqn/nRmDgaQEej7WhonozjF+Y2I/fZou/qAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;\"></div></div> <p style=\" margin:8px 0 0 0; padding:0 4px;\"> <a href=\"https://instagram.com/p/5k4HQnAB6x/\" style=\" color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;\" target=\"_top\">Dear Tahoe, You are wonderful. Love, Nico</a></p> <p style=\" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;\">A photo posted by Nicole Schuetz (@nicole) on <time style=\" font-family:Arial,sans-serif; font-size:14px; line-height:17px;\" datetime=\"2015-07-25T22:39:28+00:00\">Jul 25, 2015 at 3:39pm PDT</time></p></div></blockquote>\n<script async defer src=\"//platform.instagram.com/en_US/embeds.js\"></script>", 
-      "width"=>658, 
-      "version"=>"1.0", 
-      "author_url"=>"https://instagram.com/nicole", 
-      "author_id"=>6, 
-      "type"=>"rich"
-    } 
+    @instagram_html = get_embedded_html_instagram(link)
 
+    # EXAMPLE EMBEDDED JSON OBJECT FORMAT = {
+    #   "provider_url"=>"https://instagram.com/", 
+    #   "media_id"=>"1037200603677204145_6", 
+    #   "author_name"=>"nicole", 
+    #   "height"=>nil, 
+    #   "provider_name"=>"Instagram", 
+    #   "title"=>"Dear Tahoe, \nYou are wonderful. \nLove, Nico", 
+    #   "html"=>"<blockquote class=\"instagram-media\" data-instgrm-captioned data-instgrm-version=\"4\" style=\" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);\"><div style=\"padding:8px;\"> <div style=\" background:#F8F8F8; line-height:0; margin-top:40px; padding:50% 0; text-align:center; width:100%;\"> <div style=\" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAAGFBMVEUiIiI9PT0eHh4gIB4hIBkcHBwcHBwcHBydr+JQAAAACHRSTlMABA4YHyQsM5jtaMwAAADfSURBVDjL7ZVBEgMhCAQBAf//42xcNbpAqakcM0ftUmFAAIBE81IqBJdS3lS6zs3bIpB9WED3YYXFPmHRfT8sgyrCP1x8uEUxLMzNWElFOYCV6mHWWwMzdPEKHlhLw7NWJqkHc4uIZphavDzA2JPzUDsBZziNae2S6owH8xPmX8G7zzgKEOPUoYHvGz1TBCxMkd3kwNVbU0gKHkx+iZILf77IofhrY1nYFnB/lQPb79drWOyJVa/DAvg9B/rLB4cC+Nqgdz/TvBbBnr6GBReqn/nRmDgaQEej7WhonozjF+Y2I/fZou/qAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;\"></div></div> <p style=\" margin:8px 0 0 0; padding:0 4px;\"> <a href=\"https://instagram.com/p/5k4HQnAB6x/\" style=\" color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;\" target=\"_top\">Dear Tahoe, You are wonderful. Love, Nico</a></p> <p style=\" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;\">A photo posted by Nicole Schuetz (@nicole) on <time style=\" font-family:Arial,sans-serif; font-size:14px; line-height:17px;\" datetime=\"2015-07-25T22:39:28+00:00\">Jul 25, 2015 at 3:39pm PDT</time></p></div></blockquote>\n<script async defer src=\"//platform.instagram.com/en_US/embeds.js\"></script>", 
+    #   "width"=>658, 
+    #   "version"=>"1.0", 
+    #   "author_url"=>"https://instagram.com/nicole", 
+    #   "author_id"=>6, 
+    #   "type"=>"rich"
+    # } 
+  end
 
+  def get_embedded_html_instagram(link)
+    HTTParty.get(INSTA_OEMBED_URI + link)["html"]
   end
 
   def insta_search; end
