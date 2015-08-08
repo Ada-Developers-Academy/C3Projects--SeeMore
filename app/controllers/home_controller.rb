fa8a7f1167d4de2cb0af56854909618095cc5186
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-08 13:15:32 -0700

    added practice content to newsfeed to render embedded tweet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,20 @@ class HomeController < ApplicationController
   before_action :current_user
   before_action :require_signin, except: [:signin]
 
+  include ActionView::Helpers::OutputSafetyHelper
+
   def signin
   end
 
   def newsfeed
+    user = Followee.find(1)
+    user_id = user.native_id.to_i
+    tweet = @twitter_client.user_timeline(user_id, {count: 3}).first
+    @tweet_html = get_embed_html(tweet.id)
+    # raise
+  end
+
+  def get_embed_html(tweet_id)
+    @twitter_client.oembed(tweet_id, { omit_script: true })[:html]
   end
 end
