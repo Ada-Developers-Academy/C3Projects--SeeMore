000ddec16b814ecf40699efbd49be45ed9f37ef1
Kari <kari.lonczak@gmail.com>
2015-08-08 15:25:51 -0700

    Created subscription model scope to find active subscriptions. Tried writing a spec to test this, but it is not passing. I would love another set of eyes on this one to troubleshoot what's missing.

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe Subscription, type: :model do
       end
     end # context
   end #describe
+
+  describe ":active scope" do
+    let(:sub)   { create :subscription }
+    let(:sub2)  { create :subscription }
+    let(:sub3)  { create :subscription }
+  
+    it "selects subscriptions that do not have an unsubscribe_date" do
+      sub.unsubscribe_date = nil
+      sub2.unsubscribe_date = "Sat, 02 Aug 2015 22:12:25 UTC +00:00"
+      sub3.unsubscribe_date = "Sat, 01 Aug 2015 22:12:25 UTC +00:00"
+
+      expect(sub.unsubscribe_date).to eq nil
+      expect(sub3.unsubscribe_date).to eq "Sat, 01 Aug 2015 22:12:25 UTC +00:00"
+      expect(Subscription.count).to eq 3
+      expect(Subscription.active.count).to eq 1
+    end
+
+  end
 end # Rspec
