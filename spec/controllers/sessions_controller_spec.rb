783ddfe0c849b20e9edebb01ec42950c1baa5d84
Anita <asovay@gmail.com>
2015-08-09 10:17:06 -0700

    completed sessions controller tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+
+  before :each do
+    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:instagram]
+  end
+
   describe "GET #create" do
     context "logging in via instagram - valid params" do
-      before :each do
-        request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:instagram]
-      end
 
       it "redirects to root_path" do
         get :create, provider: :instagram
@@ -34,10 +36,6 @@ RSpec.describe SessionsController, type: :controller do
         credentials: { token: 'token' }
       } }
 
-      before :each do
-        request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:instagram]
-      end
-
       it "does not create a user" do
         get :create, invalid_params
 
@@ -45,56 +43,27 @@ RSpec.describe SessionsController, type: :controller do
           get :create, invalid_params
           }.to change(User, :count).by(0)
       end
-
     end
   end
 
-  # describe "POST #create" do
-  #   context "valid params" do
-  #     before :each do
-  #       @user = create :user
-  #       session[:user_id] = @user.id
-  #       post :create, tweet: attributes_for(:tweet)
-  #     end
+  describe "DELETE #destroy" do
+    context "valid params" do
+      before :each do
+        @user = create :user
+        session[:user_id] = @user.id
+      end
 
-  #     it "creates a twitter record" do
-  #       expect(Tweet.count).to eq(1)
-  #       expect(Tweet.first.username).to eq("Ada")
-  #     end
+      it "resets the session" do
+        delete :destroy
 
-  #     it "associates with a user" do
-  #       expect(Tweet.first.user_ids).to include(@user.id)
-  #     end
+        expect(session[:user_id]).to eq(nil)
+      end
 
-  #     it "redirects to root_path" do
-  #       expect(response).to redirect_to(root_path)
-  #       expect(response).to have_http_status(302)
-  #     end
-  #   end
-  # end
-
-  # describe "DELETE #destroy" do
-  #   context "valid params" do
-  #     before :each do
-  #       @user = create :user
-  #       session[:user_id] = @user.id
-  #       post :create, tweet: attributes_for(:tweet)
-  #     end
-
-  #     it "creates a twitter record" do
-  #       expect(Tweet.count).to eq(1)
-  #       expect(Tweet.first.username).to eq("Ada")
-  #     end
-
-  #     it "associates with a user" do
-  #       expect(Tweet.first.user_ids).to include(@user.id)
-  #     end
-
-  #     it "redirects to root_path" do
-  #       expect(response).to redirect_to(root_path)
-  #       expect(response).to have_http_status(302)
-  #     end
-  #   end
-  # end
+      it "redirects to the home page" do
+        delete :destroy
+        expect(subject).to redirect_to(root_path)
+      end
+    end
+  end
 
 end
