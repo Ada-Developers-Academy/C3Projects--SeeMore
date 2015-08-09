6f905d837bcf2d51ff5202c47456ba2cc3493d3e
Elia <elgrenier@gmail.com>
2015-08-09 14:27:40 -0700

    passing model validation specs for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
   # Associations
   belongs_to :subscription
-  has_many :users, through: :subscriptions
+  has_many :users, through: :subscription
 
   #Validations
-  validates_presence_of :username, :posted_at, :content_id
+  validates_presence_of :username, :posted_at, :content_id, :subscription_id
 end
