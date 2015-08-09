93cab64a8223706e76b835eb0b15d406074bc115
Elsa <elsa.moluf@gmail.com>
2015-08-09 15:58:10 -0700

    Pseudo-coded plan for implementing tweets saved to db in feed.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -9,18 +9,31 @@ class FeedsController < ApplicationController
       end
     end
 
+
+
+    # to get @user's feed
+    # first, find all @user's people from Tweet table
+    # for each person, retrieve all TweetPosts with that tweet_id fk, push them into a collection
+      # (if none yet, aka first time following that person, then just fetch/create from API)
+      # else, from all people's tweet_posts, find max posted_at time (most recent time)
+      # pass that time also into Twitter API call to fetch tweets made since, save those to db
+      # display tweets from db
+
+    # from those people, give Twitter API their username to retrieve their tweets
+    # for each person, call self.find_or_create_from_twitter_api on the API response to save all their tweets to the db
+
     if @user && @user.tweets
       @people = []
+      # will eventually combine Instagram and Twitter feeds into one, then sort both together by posted_at time
       # @people << Instagram.find(@user.instagram_ids)
       @people << Tweet.find(@user.tweet_ids)
       @people.flatten!
-      @people.sort_by! { |person| person.username.downcase }
+      # @people.sort_by! { |person| person.username.downcase } # Elsa's comment: why are we sorting @people? @feed will reshuffle everything later
 
       @feed = []
       @people.each do |person|
         username = person.username
         @feed << @twitter.client.user_timeline(username, count: 10)
-        raise
         @feed.flatten!
         @feed.sort_by { |tweet| tweet.created_at.strftime("%m/%d/%Y") }
       end
