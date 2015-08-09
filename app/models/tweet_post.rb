e1b29536921183529c3a9360eb94abb19ac0dcd9
Elsa <elsa.moluf@gmail.com>
2015-08-09 14:17:20 -0700

    Ensure presence of tweet_id foreign key.

--- app/models/tweet_post.rb
+++ app/models/tweet_post.rb
@@ -3,18 +3,18 @@ class TweetPost < ActiveRecord::Base
   belongs_to :tweet
 
   # Validations ----------------------------------------------
-  validates :post_id, :posted_at presence: true
+  validates :post_id, :posted_at, :tweet_id presence: true
   validates :post_id uniqueness: true
 
-  def self.find_or_create_from_twitter_api(response)
-    post_id = response["id"]
+  def self.find_or_create_from_twitter_api(twitter_api)
+    post_id = twitter_api["id"]
 
     tweet_post = TweetPost.where(post_id: post_id)
-    tweet_post.posted_at = response["created_at"]
-    tweet_post.text = response["text"]
+    tweet_post.posted_at = twitter_api["created_at"]
+    tweet_post.text = twitter_api["text"]
     tweet_post.media_url = ["entities"]["media"]["media_url"]
 
-    tweet_person = Tweet.find_by(provider_id: response["user"]["id"])
+    tweet_person = Tweet.find_by(provider_id: twitter_api["user"]["id"])
     tweet_post.tweet_id = tweet_person.id
 
     return tweet_post.save ? tweet_post : nil
