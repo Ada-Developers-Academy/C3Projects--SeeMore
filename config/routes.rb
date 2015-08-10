f0affc7f8ee2cf2875c29e69c9cabc47fcebac0a
Loraine <loraine@lorainekv.com>
2015-08-10 09:53:15 -0700

    Updated route to include twitter user id.

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
   post '/search_redirect',        to: 'feeds#search_redirect', as: 'search_redirect'
   get  '/results/:search_term',   to: 'feeds#search_results',  as: 'search_results'
 
-  post '/twitter_follow',      to: 'feeds#tw_follow',      as: 'tw_follow'
+  post '/twitter_follow/:tw_user',      to: 'feeds#tw_follow',      as: 'tw_follow'
 end
