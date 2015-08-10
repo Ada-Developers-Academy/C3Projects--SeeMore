063015e0d64b66ada571f713f600f99f2080cbeb
Brenna <brennaleker@gmail.com>
2015-08-10 15:15:42 -0700

    changed query to an instance variable so that we can use it in the view

--- app/controllers/vimeo_controller.rb
+++ app/controllers/vimeo_controller.rb
@@ -1,14 +1,14 @@
 class VimeoController < ApplicationController
    SEARCH_URI = "https://api.vimeo.com/users?per_page=30&query="
-   TOKEN_HEADER = {  
+   TOKEN_HEADER = {
                     "Accept" => "application/vnd.vimeo.*+json;version=3.2",
-                    "Authorization" => "bearer #{ENV["VIMEO_ACCESS_TOKEN"]}" 
+                    "Authorization" => "bearer #{ENV["VIMEO_ACCESS_TOKEN"]}"
                   }
 
   def results
-    query = params[:query]
-                
-    auth = JSON.parse(HTTParty.get(SEARCH_URI + query + "&sort=relevant", :headers => TOKEN_HEADER ))
+    @query = params[:query]
+
+    auth = JSON.parse(HTTParty.get(SEARCH_URI + @query + "&sort=relevant", :headers => TOKEN_HEADER ))
     @results = auth["data"]
   end
 end
