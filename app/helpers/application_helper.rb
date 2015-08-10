9df920902795997684196a1607cca40785e49970
Loraine <loraine@lorainekv.com>
2015-08-10 14:19:48 -0700

    Fixed our followed? check based on our new schema.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,14 @@
 module ApplicationHelper
-
   def tw_following?(id)
     # This checks to see if tweets by this twitter user exist
-    matches = Tweet.find_by(tw_user_id_str: id)
+    matches = TwUser.find_by(tw_user_id_str: id)
     # Checks if there are any matches
     unless matches == nil
-    # This checks if that collection of matches are associated with
-    # the Creep Peep user
-    matches.first.user_id == session[:user_id]
-      # This conditional might not be necessary...
-      # if matches.first.user_id == session[:user_id]
-      #   return true
-      # else
-      #   return false
-      # end
+    # This checks the twitter user is associated with the Creep Peep user
+    matches.users.first.id == session[:user_id]
+    # if match, this returns true
     else
       return false
     end
   end
-
 end
