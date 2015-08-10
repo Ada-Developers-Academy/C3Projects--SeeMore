c1e7314ccf7249996ec0b59922ed517e3b082dfb
Corinne Pingul <corinnepingul@gmail.com>
2015-08-10 16:50:10 -0700

    WIP.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ActiveRecord::Base
 
   validates :uid, :post_time, :prey_id, :url, :provider, presence: true
 
+  # TWEETS --------------------------------------------------------------------
+
   def self.seed_tweets(prey_uid, count = 5)
     tweets = TwitterClient.fetch_tweets(prey_uid, { count: count })
     create_many_from_api(tweets)
@@ -24,6 +26,17 @@ class Post < ActiveRecord::Base
         Medium.create(url: medium.media_url_https.to_s, post_id: post_id)
       end
     end
+  end
+
+  # GRAMS --------------------------------------------------------------------
+
+  def self.seed_grams(user_uid)
+    grams = InstagramClient.seed_grams(user_uid)
+    create_many_grams_from_api(grams)
+  end
+
+  def self.create_many_grams_from_api(grams)
+    #WIP
   end
 
   private
--- lib/instagram_client.rb
+++ lib/instagram_client.rb
@@ -1,8 +1,21 @@
 class InstagramClient
   INSTAGRAM_SEARCH_USERS_URI = "https://api.instagram.com/v1/users/search?client_id=#{ENV["INSTAGRAM_CLIENT_ID"]}&"
+  INSTAGRAM_FETCH_GRAMS_URI = "https://api.instagram.com/v1/users/"
+  SEED_COUNT = 5
 
   def self.user_search(search_term)
     results = HTTParty.get(INSTAGRAM_SEARCH_USERS_URI + "q=#{search_term}")
     return results["data"]
+  end
+
+  def self.seed_grams(user_uid)
+    result = HTTParty.get(INSTAGRAM_FETCH_GRAMS_URI + "#{user_uid}/media/recent/?access_token=#{ENV["INSTAGRAM_ACCESS_TOKEN"]}&count=#{SEED_COUNT}")
+    return result["data"] # this returns an array of hashes with information
+    # about the grams...each index in the array corresponds to a new gram
+  end
+
+  def self.update_grams(user_uid, last_gram_id)
+    result = HTTParty.get(INSTAGRAM_FETCH_GRAMS_URI + "#{user_uid}/media/recent/?access_token=#{ENV["INSTAGRAM_ACCESS_TOKEN"]}&min_id=#{last_gram_id}")
+    return result["data"]
   end
 end
