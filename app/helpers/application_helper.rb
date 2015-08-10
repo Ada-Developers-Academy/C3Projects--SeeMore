1ac0ee46e353a90b539fa33d006f9ea76c9add1f
Loraine <loraine@lorainekv.com>
2015-08-10 15:41:19 -0700

    Lindsey and I made the following? check way shorter and straightforward.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,7 @@
 module ApplicationHelper
   def tw_following?(id)
-    # This checks to see if tweets by this twitter user exist
-    matches = TwUser.find_by(tw_user_id_str: id)
-    # Checks if there are any matches
-    unless matches == nil
-      # This checks the twitter user is associated with the Creep Peep user
-      matches.users.first.id == session[:user_id]
-      # if match, this returns true
-    else
-      return false
-    end
+    our_user = User.find(session[:user_id])
+    matches = our_user.tw_users.where(tw_user_id_str: id)
+    matches.exists?
   end
 end
