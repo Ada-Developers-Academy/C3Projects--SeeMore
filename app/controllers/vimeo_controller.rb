02594c8ba02b0e1e5ba157e07f7b2ca96db2b220
Katie Atrops <katrops@gmail.com>
2015-08-10 14:50:05 -0700

    working vimeo api(searching for users)

--- app/controllers/vimeo_controller.rb
+++ app/controllers/vimeo_controller.rb
@@ -1,9 +1,16 @@
 class VimeoController < ApplicationController
    SEARCH_URI = "https://api.vimeo.com/users?per_page=30&query="
-
+   TOKEN_HEADER = {  
+                    "Accept" => "application/vnd.vimeo.*+json;version=3.2",
+                    "Authorization" => "bearer #{ENV["VIMEO_ACCESS_TOKEN"]}" 
+                  }
+   
+   
   def results
     user = params[:query]
-    auth = HTTParty.get(SEARCH_URI + user + "&sort=relevant")
+                
+    auth = JSON.parse(HTTParty.get(SEARCH_URI + user + "&sort=relevant", :headers => TOKEN_HEADER ))
+    @results = auth["data"]
   end
 
 
