e50f4b5b1268dba5383fa624b3060519dbe5d46f
Corinne Pingul <corinnepingul@gmail.com>
2015-08-10 13:21:26 -0700

    Updates all files with new model name (posts and media).

--- spec/factories/tweet_media.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :tweet_medium do
-    url "MyString"
-    tweet_id 1
-  end
-end
--- spec/factories/tweets.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :tweet do
-    uid "MyString"
-    body "MyString"
-    url "MyString"
-    post_time "2015-08-07 14:36:31"
-    prey_id 1
-  end
-end
