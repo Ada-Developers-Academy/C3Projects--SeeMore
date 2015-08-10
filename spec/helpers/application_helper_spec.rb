cc1526d73c96dd4997c33df8b5af22855d1fd1e6
Loraine <loraine@lorainekv.com>
2015-08-10 15:02:42 -0700

    Wrote Application Helper specs.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -12,11 +12,28 @@ require 'rails_helper'
 # end
 RSpec.describe ApplicationHelper, type: :helper do
 
-  context "when you're already following a Twitter user" do
-    
+  before(:each) do
+    create :user
   end
 
   context "when you haven't followed a Twitter user yet" do
+    it "returns false after a following? check" do
+      # If a user has just signed in / signed up and searched for Twitter users
+      expect(helper.tw_following?("100")).to be(false)
+    end
   end
+
+  context "when you're already following a Twitter user" do
+    it "returns true after a following? check" do
+      create :tw_user
+      our_user = User.find(1)
+      session[:user_id] = our_user.id
+      twitter_user = TwUser.find(1)
+      our_user.tw_users << [twitter_user]
+      expect(helper.tw_following?("100")).to be(true)
+    end
+  end
+
+
 
 end
