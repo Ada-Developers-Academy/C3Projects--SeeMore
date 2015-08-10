cdc5796bc5a734c3df0550a995d02d3c64655d6a
Anita <asovay@gmail.com>
2015-08-10 16:44:52 -0700

    reverting to old twitter call in Twit model

--- lib/twit.rb
+++ lib/twit.rb
@@ -1,16 +1,10 @@
 class Twit
-  CLIENT = Twitter::REST::Client.new do |config|
+  attr_reader :client
+
+  @client = Twitter::REST::Client.new do |config|
     config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
     config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
     config.access_token = ENV['TWITTER_ACCESS_TOKEN']
     config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-  end
-
-  def self.user_search(search_term)
-    CLIENT.user_search(search_term)
-  end
-
-  def self.user_timeline(search_term)
-    CLIENT.user_timeline(search_term)
   end
 end
