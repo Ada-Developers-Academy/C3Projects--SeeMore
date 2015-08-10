4d7c0039bff82ba0d7b14a6239b3f73e58eee9a9
Ashley Watkins <AWatkins31@gmail.com>
2015-08-09 18:18:42 -0700

    Adds pending messages to prey for tests that require API data

--- spec/models/prey_spec.rb
+++ spec/models/prey_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Prey, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  pending "#update_tweets"
+  pending "seeds tweets after create"
 end
