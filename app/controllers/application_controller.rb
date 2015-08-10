85e62aaebb5efb2ac879c934237b14e18fdf9baf
Ashley Watkins <AWatkins31@gmail.com>
2015-08-09 18:36:36 -0700

    Adds universal login requirement for all views except the login page.
    Also updates specs to set the session[:stalker_id] before all tests.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :require_login
+
+  def require_login
+    redirect_to landing_path unless session[:stalker_id]
+  end
 end
