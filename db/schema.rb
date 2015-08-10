0a620f9eca06ebe1f40be9c842e954658a9c2fc4
Loraine <loraine@lorainekv.com>
2015-08-10 10:24:22 -0700

    Twitter and User join table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807165656) do
+ActiveRecord::Schema.define(version: 20150810172051) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "tw_user_id_str"
@@ -25,8 +25,15 @@ ActiveRecord::Schema.define(version: 20150807165656) do
     t.integer  "tw_favorite_count"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
-    t.integer  "user_id"
   end
+
+  create_table "tweets_users", id: false, force: :cascade do |t|
+    t.integer "tweet_id", null: false
+    t.integer "user_id",  null: false
+  end
+
+  add_index "tweets_users", ["tweet_id", "user_id"], name: "index_tweets_users_on_tweet_id_and_user_id"
+  add_index "tweets_users", ["user_id", "tweet_id"], name: "index_tweets_users_on_user_id_and_tweet_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
