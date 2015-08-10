6ffce731e43e184a3fe881292623be50049f7c6f
Jeri <drvonnjerryxlii@gmail.com>
2015-08-10 09:02:15 -0700

    wip updating feeds

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,7 @@ class Post < ActiveRecord::Base
   # Scopes ---------------------------------------------------------------------
   scope :chronological, -> { order("date_posted DESC") }
   scope :only_thirty, -> { chronological.limit(30) }
+  # scope :paginate, -> (page_number=1) { paginating magic here? }
   # TODO: consider putting chronological's actual code into only_thirty after
   # addressing below comment re: pagination
   # OPTIMIZE: maybe we should account for pagination? we would need something
