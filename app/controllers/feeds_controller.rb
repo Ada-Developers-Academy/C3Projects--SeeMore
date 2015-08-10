556aee31da00a32c4698221e849f2d2bc7e23cae
marleigh <camarleigh@gmail.com>
2015-08-10 13:58:30 -0700

    created ig follow method(empty).

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -28,4 +28,11 @@ class FeedsController < ApplicationController
       redirect_to search_path(params[:provider])
     end
   end
+
+  def ig_follow
+    #id from user session present, need ig user id,
+    #refresh back to search result page
+
+    
+  end
 end
