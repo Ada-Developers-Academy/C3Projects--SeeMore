4141aa8162d3dd530b08bcffbb071b890bf012a6
amirahaile <amira.dhaile@gmail.com>
2015-08-10 16:14:15 -0700

    Created .latest_posts(limit) scope for IG posts.

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,4 +1,7 @@
 class Gram < ActiveRecord::Base
   #Associations
   belongs_to :instagram_user
+
+# Scopes -----------------------------------------------------------------------
+  scope :latest_posts, ->(limit) { order('created_at DESC').limit(limit) }
 end
--- spec/models/gram_spec.rb
+++ spec/models/gram_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Gram, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "scope #latest_posts(limit)" do
+    before :each do
+      3.times do
+        create :gram
+      end
+
+      @post4 = create :gram
+      @post5 = create :gram
+    end
+
+    it "retrieves the latest posts" do
+      posts = Gram.latest_posts(2)
+
+      expect(posts).to include @post4
+      expect(posts).to include @post5
+    end
+
+    it "returns number of posts according to limit parameter" do
+      three_posts = Gram.latest_posts(3)
+      one_post = Gram.latest_posts(1)
+
+      expect(three_posts.count).to eq 3
+      expect(one_post.count).to eq 1
+    end
+
+    it "returns posts in reverse chronological order" do
+      posts = Gram.latest_posts(3)
+
+      expect(posts[0].created_at).to be > posts[1].created_at
+      expect(posts[1].created_at).to be > posts[2].created_at
+      expect(posts[2].created_at).to be < posts[0].created_at
+    end
+  end
 end
