ae7608443d815049a662f9abc9a3abf9fb163900
amirahaile <amira.dhaile@gmail.com>
2015-08-10 14:41:50 -0700

    Started self.first_or_create_account and fixed object passed via follow button.

--- app/models/instagram_user.rb
+++ app/models/instagram_user.rb
@@ -1,4 +1,8 @@
 class InstagramUser < ActiveRecord::Base
   #Associations
   has_and_belongs_to_many :users
+
+# Methods ----------------------------------------------------------------------
+  def self.first_or_create_account(ig_user)
+  end
 end
--- spec/models/instagram_user_spec.rb
+++ spec/models/instagram_user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe InstagramUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#find_or_create_account" do
+    before :each do
+      @ig_user = create :instagram_user
+      @account_info = {
+        username: 'OOP4lyfe',
+        profile_pic: 'oop.jpg',
+        ig_user_id: '3948820',
+        fullname: 'Sandy Metz'
+      }
+    end
+
+    context "the IG account exists in the database" do
+      it "returns account's corresponding InstagramUser object" do
+        account = InstagramUser.find_or_create_account(@ig_user)
+        expect(account).to eq @ig_user
+      end
+    end
+
+    context "the IG account doesn't exist in the database" do
+      it "create an InstagramUser object" do
+        account = InstagramUser.find_or_create_account(@account_info)
+        expect(account).to be_instance_of(InstagramUser)
+      end
+    end
+  end
 end
