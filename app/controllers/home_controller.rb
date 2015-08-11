df053debb108b45b6799c73db602950e2b8c6238
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-10 17:56:41 -0700

    fixed instagram portion of get_new_posts
    wrote find_instagram_params, fixed name of method in call from find_twitter_params to find_instagram_params
    added logic to newsfeed to create variables for display for debugging

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,12 +18,23 @@ class HomeController < ApplicationController
     # tweet = @twitter_client.user_timeline(user_id, { count: 2 }).last
     # @tweet_html = get_embed_html(tweet.id)
     # raise
+    #### uncomment below for debugging example code
+    @posts = []
+    @current_user.followees.each do |f|
+      f.posts.each do |p|
+        @posts << p.embed_html
+      end
+    end
   end
 
-  def get_embed_html(tweet_id)
+  def get_twitter_embed_html(tweet_id)
     @twitter_client.oembed(tweet_id, { omit_script: true })[:html]
   end
 
+  def get_instagram_embed_html(post)
+    link = post["link"]
+    HTTParty.get(INSTA_OEMBED_URI + link)["html"]
+  end
 
   def get_new_posts
     active_subscriptions = @current_user.subscriptions.active
@@ -43,7 +54,7 @@ class HomeController < ApplicationController
         end
       elsif source == "instagram"
         posts.each do |post|
-          post_hash = find_twitter_params(post, followee)
+          post_hash = find_instagram_params(post, followee)
           Post.create(post_hash)
         end
       end
@@ -62,11 +73,21 @@ class HomeController < ApplicationController
     post_hash[:native_created_at] = post.created_at
     post_hash[:followee_id] = followee.id
     post_hash[:source] = followee.source
-    post_hash[:embed_html] = get_embed_html(post.id)
+    post_hash[:embed_html] = get_twitter_embed_html(post.id)
 
     return post_hash
   end
 
+  def find_instagram_params(post, followee)
+    post_hash = {}
+    post_hash[:native_id] = post["id"]
+    post_hash[:native_created_at] = convert_instagram_time(post["created_time"])
+    post_hash[:followee_id] = followee.id
+    post_hash[:source] = followee.source
+    post_hash[:embed_html] = get_instagram_embed_html(post)
+    
+    return post_hash
+  end
 
   # do we want to pass in followee or followee_id?
   def get_posts_from_API(source, followee, last_post_id)
@@ -84,7 +105,7 @@ class HomeController < ApplicationController
           INSTA_USER_POSTS_URI + followee.native_id + "/media/recent/?min_id=" + last_post_id + "&access_token=" + ENV["INSTAGRAM_ACCESS_TOKEN"])
       else
         response = HTTParty.get(
-          INSTA_USER_POSTS_URI + followee.native_id + "/media/recent/?count=" + FIRST_POSTS_NUM.to_s + "&access_token=" + ENV["INSTAGRAM_ACCESS_TOKEN"])
+          INSTA_USER_POSTS_URI + followee.native_id + "/media/recent/?count=" + "5" + "&access_token=" + ENV["INSTAGRAM_ACCESS_TOKEN"])
       end
         posts = response["data"]
     end
