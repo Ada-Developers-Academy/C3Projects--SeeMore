38e53b94d7705de1308b0f2ab1055b92994df6b0
amirahaile <amira.dhaile@gmail.com>
2015-08-11 11:26:49 -0700

    Deleted commented out method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,6 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
 
 #Methods -----------------------------------------------------------------------
-  # def self.find_or_create_from_instagram(response)
-  #   auth_uid = response['id']
-  #   auth_provider = 'instagram'
-  #
-  #   user = User.where(uid: auth_uid, provider: auth_provider).first_or_initialize
-  #   user.username = response['data']['username']
-  #   user.avatar_url = response['data']['profile_picture']
-  #
-  #   return user.save ? user : nil
-  # end
-
-
   def self.find_or_create_from_omniauth(auth_hash)
     instagram = auth_hash["user"]
     auth_uid = instagram.nil? ? "nil" : instagram["id"]
