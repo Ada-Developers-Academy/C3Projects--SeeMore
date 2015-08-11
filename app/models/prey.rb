03493279c801230ffc08c38bf51f4f17cd15e0e1
Corinne Pingul <corinnepingul@gmail.com>
2015-08-11 16:46:10 -0700

    Refactors scope.

--- app/models/prey.rb
+++ app/models/prey.rb
@@ -7,7 +7,7 @@ class Prey < ActiveRecord::Base
   validates :uid, :provider, presence: true
   after_create :seed_posts
 
-  scope :last_post_uid, -> (prey_uid) { Prey.find_by(uid: prey_uid).posts.order(post_time: :desc).first.uid }
+  scope :last_post_uid, -> (prey_uid) { Prey.find_by(uid: prey_uid).posts.maximum(:uid) }
 
   def update_posts
     Post.update_tweets(self) if tweeter?
