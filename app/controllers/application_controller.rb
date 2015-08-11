0903370522b6d1ddeb53dd70a5f189d615a0ae33
amirahaile <amira.dhaile@gmail.com>
2015-08-11 14:06:01 -0700

    Created universal instance variable for IG client.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  
   before_filter :twit_init, :ig_client
   helper_method :client
 
