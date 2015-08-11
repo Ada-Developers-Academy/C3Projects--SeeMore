834cb647eeee802edfecdd0b8a8ee960c4ec3522
Loren Wang <wang131.131@gmail.com>
2015-08-11 11:42:28 -0700

    fixin feed index with both media posts

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,52 +5,39 @@ class FeedsController < ApplicationController
 
   def index
     @user = User.find_by(id: session[:user_id])
+
     @posts = []
     if @user && @user.instagrams
       @user.instagram_posts.each do |post|
         @posts << HTTParty.get(INSTAGRAM_URI + "media/" + post.post_id + "?access_token=#{session[:access_token]}")
       end
-      @posts.sort_by! { |post| post["data"]["created_time"]}
-      @posts.reverse!
+      @posts.flatten!
     end
 
+    @feed = []
     if @user && @user.tweets
-      # @people = []
-      # # will eventually combine Instagram and Twitter feeds into one, then sort both together by posted_at time
-      # # @people << Instagram.find(@user.instagram_ids)
-      # @people << Tweet.find(@user.tweet_ids)
-      # @people.flatten!
-
-      @feed = []
-      # @people.each do |person|
       usernames = @user.tweets.map &:username
       usernames.each do |username|
-        # username = person.username
         @feed << @twitter.client.user_timeline(username)
-        # raise
-
       end
-      # raise
       @feed.flatten!
     end
-
 
     Struct.new("Ninja", :username, :photo, :text, :date_time, :avatar, :provider, :link)
 
     @all_posts = []
-
     @posts.each do |ig_post|
       username = ig_post["data"]["user"]["username"]
       photo = ig_post["data"]["images"]["standard_resolution"]["url"]
+      date_time = Time.at(ig_post["data"]["created_time"].to_i)
+      avatar = nil
+      provider = "Instagram"
+      link = ig_post["data"]["link"]
       unless ig_post["data"]["caption"].nil?
         text = ig_post["data"]["caption"]["text"]
       else
         text = ""
       end
-      date_time = Time.at(ig_post["data"]["created_time"].to_i)
-      avatar = nil
-      provider = "Instagram"
-      link = ig_post["data"]["link"]
 
       @all_posts.push(Struct::Ninja.new(username, photo, text, date_time, avatar, provider, link))
     end
@@ -59,7 +46,7 @@ class FeedsController < ApplicationController
       username = tweet_post.user.screen_name
       text = tweet_post.text
       date_time = tweet_post.created_at
-      photo = nil
+      photo = nil #tweet_post.profile_image_url(size = :bigger)
       avatar = nil
       provider = "Twitter"
       link = nil
