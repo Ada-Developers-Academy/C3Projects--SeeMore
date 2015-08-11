97e1f79502aeb6cafb79bdb4d4a5e07127e323ed
Shanna Chau <shannachau@gmail.com>
2015-08-11 11:18:15 -0700

    Wrapped some routes so that they only exist when we're in development. Also regrouped routes to organize

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,23 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  get "/auth/:provider/callback", to: "sessions#create"
-  post "/auth/:provider/callback", to: "sessions#create"# this is required for the OmniAuth Developer Strategy
+  if Rails.env.development?
+    post "/auth/:provider/callback", to: "sessions#create"# this is required for the OmniAuth Developer Strategy
+    get "auth/developer" => 'sessions#create'
+  end
 
   get "/auth/:provider", to: "sessions#create"
   post "/auth/:provider", to: "sessions#create", as: 'login'
+  get "/auth/:provider/callback", to: "sessions#create"
 
   get "/login", to: "sessions#index", as: 'landing'
+  delete "logout" => 'sessions#destroy', as: 'logout'
+
+  get "dashboard/:stalker_id" => "stalkers#index", as: "dashboard"
 
   post "/search", to: "searches#search"
   get "search/:client/:search_term", to: "searches#show", as: "search_results"
 
-  get "auth/developer" => 'sessions#create'
-
-  delete "logout" => 'sessions#destroy', as: 'logout'
-
   post "/follow" => 'prey#create', as: 'follow'
-
-  get "dashboard/:stalker_id" => "stalkers#index", as: "dashboard"
-
   patch "/unfollow", to: "prey#unfollow", as: "unfollow"
-
 end
