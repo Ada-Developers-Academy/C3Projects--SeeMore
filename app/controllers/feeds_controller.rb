97fbd02fc8841485160c596d010dd37e42b881fa
amirahaile <amira.dhaile@gmail.com>
2015-08-10 19:37:27 -0700

    Added #ig_follow back for routing purposes.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,19 @@
 class FeedsController < ApplicationController
-  def index; end
+  def index
+    # user = User.find(session[:user_id])
+    # instagram_client = Instagram.client(:access_token => session[:access_token])
+    # instagram_posts = []
+
+    # user.instagram_users.each do |ig_user|
+    #   last_post_id = ig_user.grams.last.ig_id ||= nil
+    #   unless last_post_id == nil
+    #     posts = instagram_client.user_recent_media(ig_user.ig_user_id, { min_id: last_post_id })
+    #     raise
+    #   else
+    #     instagram_client.user_recent_media(ig_user.ig_user_id, { count: 5 })
+    #   end
+    # end
+  end
 
   def search; end
 
@@ -27,5 +41,12 @@ class FeedsController < ApplicationController
       # TODO: send flash notices
       redirect_to search_path(params[:provider])
     end
+  end
+
+  def ig_follow
+    user = User.find(session[:user_id])
+    ig_account = params # hash of info
+    ig_account = user.ig_follow(ig_account) # InstagramAccount obj
+    redirect_to :back
   end
 end
