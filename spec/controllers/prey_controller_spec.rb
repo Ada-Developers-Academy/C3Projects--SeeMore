4dc3b2a93d5d65e9d71d9cdae5c917fcdc944264
Ashley Watkins <AWatkins31@gmail.com>
2015-08-11 14:13:44 -0700

    Adds pending tests + comments for prey controller

--- spec/controllers/prey_controller_spec.rb
+++ spec/controllers/prey_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe PreyController, type: :controller do
-  pending "Tests For Prey Controller"
+  pending "tests for PreyController#create"
+  # flash messages
+  # does not create new prey if the prey is already in the db
+  # creates a new prey if the prey is not already in the db
+  # redirects to root path
+  # works for both twitter and instagram
+
+  pending "tests for PreyController#unfollow"
+  # flash messages
+  # does not delete the prey from the db
+  # removes the association
+  # redirects to dashboard_path(session[:stalker_id])
+  # works for both twitter and instagram
+
 end
