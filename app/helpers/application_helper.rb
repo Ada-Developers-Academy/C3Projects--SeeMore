e16c96c9b505d11b19fbd5dc88d4aa375375fdcd
Jeri <drvonnjerryxlii@gmail.com>
2015-08-11 15:46:58 -0700

    more styling tweaks n_n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def only_250_characters(some_long_text)
+    if some_long_text.length > 250
+      return some_long_text[0,425] + "..."
+    else
+      return some_long_text
+    end
+  end
 end
