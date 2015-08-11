c1e9005400d30a40721cbd7b799cb4d8d10a7290
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-11 14:13:07 -0700

    Added a conditional so it  will get the last 5 tweet from a new followee that doesn't have any tweets in the database yet.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -9,26 +9,29 @@ class Tweet < ActiveRecord::Base
   # Methods
   def self.update_timeline(user)
     followees = user.tw_users
-
+    @twit_init = TwitInit.new
     followees.each do |followee|
       # the twitter id of the followee as an integer
       id = followee.tw_user_id_str.to_i
-      # the twitter id of the last saved tweet by that followee
-      last_id = Tweet.find_by(tw_user_id_str: id).last.tw_id_str.to_i
-      # fetches their recent tweets
-      recent_tweets = @twit_init.client.user_timeline(id, since_id: last_id)
 
-      tweet_factory
+      if followee.tweets.count == 0
+        # get the last 5 tweets and save them to the database for a new followee
+        recent_tweets = @twit_init.client.user_timeline(id).take(5)
+      else
+        # the twitter id of the last saved tweet by that followee
+        last_id = Tweet.find_by(tw_user_id_str: id).last.tw_id_str.to_i
+        # fetches their recent tweets
+        recent_tweets = @twit_init.client.user_timeline(id, since_id: last_id)
+      end
+      self.tweet_factory(recent_tweets)
     end
   end
 
-  private
-
-  def tweet_factory
+  def self.tweet_factory(recent_tweets)
     recent_tweets.each do |tweet|
       our_tweet = Tweet.new
       our_tweet.tw_user_id_str = tweet.user.id
-      our_tweet.tw_id_str = tweet.id_str
+      our_tweet.tw_id_str = tweet.id.to_s
       our_tweet.tw_text = tweet.text
       our_tweet.tw_created_at = tweet.created_at
       our_tweet.tw_retweet_count = tweet.retweet_count
