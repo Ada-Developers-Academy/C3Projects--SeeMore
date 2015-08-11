d75283388ce5863ccc38284d0907a340e7811cfa
Elia <elgrenier@gmail.com>
2015-08-11 14:52:54 -0700

    moved the twitter client into the twitter controller and refactored to not call it in each method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,16 +14,6 @@ class ApplicationController < ActionController::Base
       end
     end
 
-
-  def twitter_api_object
-    Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_CLIENT_ID"]
-      config.consumer_secret     = ENV["TWITTER_CLIENT_SECRET"]
-      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
-    end
-  end
-
     def redirect_if_not_allowed
       unless logged_in?
         flash[:error] = "You must be logged in to see that."
