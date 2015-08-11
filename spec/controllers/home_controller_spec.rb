9a44bda26fe83bc455145b145e684c701199f21b
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-11 13:54:10 -0700

    added first test for #get_new_posts

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'support/vcr_setup'
 
 RSpec.describe HomeController, type: :controller do
   let(:user) { create :user }
@@ -25,5 +26,30 @@ RSpec.describe HomeController, type: :controller do
     #   session[:user_id] = nil
     #   expect(@current_user).to be_nil
     # end
+  end
+
+  # describe "get_posts_from_API" do
+  #   let(:followee) { create :followee }
+
+  #   it "returns a collection of posts" do
+  #     VCR.use_cassette 'controller/home_controller/get_posts_from_API' do
+  #       posts = get_posts_from_API(followee)
+  #       expect(posts).to_not be_nil
+  #     end
+  #   end
+  # end
+
+  describe "POST #get_new_posts" do
+    let(:followee) { create :followee }
+
+    context "updating feed after following new user" do
+      it "adds posts to the database" do
+        VCR.use_cassette 'controller/home_controller/get_new_posts' do
+          create :subscription, followee_id: followee.id, user_id: user.id
+          post :get_new_posts
+          expect(Post.count).to eq 5
+        end
+      end
+    end
   end
 end
