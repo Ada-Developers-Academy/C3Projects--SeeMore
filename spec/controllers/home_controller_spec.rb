a43d52dcf408903d9df49e1cb94c8bf036bbdd39
Kari <kari.lonczak@gmail.com>
2015-08-11 14:47:06 -0700

    Wrote test for when last_post_id is present in twitter and instagram post updates. Created two vcr cassettes-- one for last_post_id present and one for last_post_id absent.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -40,17 +40,34 @@ RSpec.describe HomeController, type: :controller do
   # end
 
   describe "POST #get_new_posts" do
-    let(:followee_twitter) { create :followee }
-    let(:followee_instagram) { create :followee, handle: "badgalriri", source: "instagram", native_id: 25945306 }
 
-    context "updating feed after following new user" do
+    context "updating feed for a new followee" do
+      let(:followee_twitter) { create :followee }
+      let(:followee_instagram) { create :followee, handle: "badgalriri", source: "instagram", native_id: "25945306" }
+      
       it "adds posts to the database" do
-        VCR.use_cassette 'controller/home_controller/get_new_posts' do
+        VCR.use_cassette 'controller/home_controller/get_new_posts_last_post_id_absent' do
           create :subscription, followee_id: followee_twitter.id, user_id: user.id
           create :subscription, followee_id: followee_instagram.id, user_id: user.id
 
           post :get_new_posts
           expect(Post.count).to eq 10
+        end
+      end
+    end
+
+    context "updating feed for an existing followee" do
+      let(:followee_twitter) { create :followee, handle: "rhomieux", native_id: "11583102", last_post_id: "630946246803361792"} # 3 back
+      let(:followee_instagram) { create :followee, handle: "barrackobama", native_id: "10206720", last_post_id: "914665339329635845_10206720", source: "instagram" } # 3 back
+
+      it "adds posts to the database" do
+        VCR.use_cassette 'controller/home_controller/get_new_posts_last_post_id_present' do
+          create :subscription, followee_id: followee_twitter.id, user_id: user.id
+          create :subscription, followee_id: followee_instagram.id, user_id: user.id
+
+          post :get_new_posts
+          expect(Post.where(source: "twitter").count).to eq 2
+          # expect(Post.where(source: "instagram").count).to eq 2
         end
       end
     end
