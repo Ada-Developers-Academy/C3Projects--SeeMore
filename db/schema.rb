c3eb8edb38d70b627616d2cb9b08a031a4a420c0
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-11 13:44:35 -0700

    Added foreign key to associate tweets to a tw_user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811202335) do
+ActiveRecord::Schema.define(version: 20150811204309) do
 
   create_table "tw_users", force: :cascade do |t|
     t.string   "tw_user_id_str"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150811202335) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.string   "tw_user_id_str"
+    t.integer  "tw_user_id"
   end
 
   create_table "users", force: :cascade do |t|
