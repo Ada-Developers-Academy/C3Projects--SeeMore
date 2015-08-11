f7a69ca59dcf08416d4833e8a779aa05ebc08a61
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-11 14:43:35 -0700

    WIP: newsfeed rspecs.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-  let(:user) { create :user }
-  before :each do
-    session[:user_id] = user.id
-  end
+  # before :each do
+  #   session[:user_id] = user.id
+  # end
 
   describe "GET #newsfeed" do
+    # let!(:souly) { create :user, id: 20 }
+    # let!(:buzz) { create :followee, id: 15 }
+    # let!(:post) { create :post, followee_id: 15 }
+    # let!(:post1) { create :post, followee_id: 15 }
+    # let!(:subscription) { create :subscription, followee_id: 15, user_id: 20 }
 
     before :each do
       get :newsfeed
@@ -16,38 +20,14 @@ RSpec.describe HomeController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-    # it "finds the current user" do
-    #   binding.pry
-    #   expect(@current_user).to eq user
-    # end
+    context "user has no subscription" do
+      let!(:souly) { create :user, id: 2 }
+      it "should display a flash error" do
+        get :newsfeed
+        binding.pry
+        expect(flash[:errors]).to include("You have no subscriptions! Search users to subscribe to.")
+      end
+    end
 
-    # it "doesn't find the current user if none set" do
-    #   session[:user_id] = nil
-    #   expect(@current_user).to be_nil
-    # end
-  end
-
-   describe "GET #refresh" do
-     context "renders newsfeed template" do
-       it "redirects" do
-         get :refresh
-         expect(response).to render_template(:newsfeed)
-       end
-     end
-   end # refresh
-
-   describe "#find_subscription_posts" do
-     let!(:souly) { create :user, id: 20 }
-     let!(:buzz) { create :followee, id: 15 }
-     let!(:post) { create :post, followee_id: 15 }
-     let!(:post1) { create :post, followee_id: 15 }
-
-     it "finds all user's subscription posts" do
-      session[:user_id] = souly.id
-      @current_user = souly
-      post
-      get :refresh
-      expect(@all_posts).to eq(1)
-     end
-   end
+  end # newsfeed
 end # describe
