01a2a5cf28f2fb8f9d59c75cd4e6a4a2467b65f0
Shanna Chau <shannachau@gmail.com>
2015-08-11 13:15:24 -0700

    Consolidated routes and changed everything to double quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,21 @@
 Rails.application.routes.draw do
-  root 'posts#index'
+  root "posts#index"
 
   if Rails.env.development?
     post "/auth/:provider/callback", to: "sessions#create"# this is required for the OmniAuth Developer Strategy
-    get "auth/developer" => 'sessions#create'
   end
 
-  get "/auth/:provider", to: "sessions#create"
-  post "/auth/:provider", to: "sessions#create", as: 'login'
+  post "/auth/:provider", to: "sessions#create", as: "login"
   get "/auth/:provider/callback", to: "sessions#create"
 
-  get "/login", to: "sessions#index", as: 'landing'
-  delete "logout" => 'sessions#destroy', as: 'logout'
+  get "/login", to: "sessions#index", as: "landing"
+  delete "/logout" => "sessions#destroy", as: "logout"
 
-  get "dashboard/:stalker_id" => "stalkers#index", as: "dashboard"
+  get "/dashboard/:stalker_id" => "stalkers#index", as: "dashboard"
 
   post "/search", to: "searches#search"
-  get "search/:client/:search_term", to: "searches#show", as: "search_results"
+  get "/search/:client/:search_term", to: "searches#show", as: "search_results"
 
-  post "/follow" => 'prey#create', as: 'follow'
+  post "/follow" => "prey#create", as: "follow"
   patch "/unfollow", to: "prey#unfollow", as: "unfollow"
 end
