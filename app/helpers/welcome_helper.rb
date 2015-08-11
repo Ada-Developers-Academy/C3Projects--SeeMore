bab24880537fd374a6535dd5f1b4e52ca49e53c8
Jeri <drvonnjerryxlii@gmail.com>
2015-08-11 15:56:07 -0700

    COMMAS

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,9 @@
 module WelcomeHelper
+  def shorten(some_long_text) # FIXME: test this method!
+    if some_long_text.length > 250
+      return some_long_text[0,425] + "..."
+    else
+      return some_long_text
+    end
+  end
 end
