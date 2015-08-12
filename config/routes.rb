f1bde0592d68a91221bf94f1fb7e3fb74e12e383
Jeri <drvonnjerryxlii@gmail.com>
2015-08-12 16:12:58 -0700

    added routes for unsubscribe buttons & removed old routes for omniauth developer strategy

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,21 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   # authorization routes -------------------------------------------------------
-  # omniauth developer strategy
-  get "/auth/developer", as: "sign_up"
-  post "/auth/developer/callback", to: "sessions#create"
-  # OPTIMIZE: should we delete or uncomment this :provider callback route? we're not using it at present.
-  # get "/auth/:provider/callback", to: "sessions#create"
-  # omniauth production strategy
-  get "/auth/instagram/callback" => 'sessions#create_instagram'
-  get "/auth/vimeo/callback" => 'sessions#create_vimeo'
-  # logging out
+  get "/auth/instagram/callback" => "sessions#create_instagram"
+  get "/auth/vimeo/callback" => "sessions#create_vimeo"
   delete "/logout", to: "sessions#destroy", as: "logout"
 
-  # feed routes
+  # feed routes ----------------------------------------------------------------
+  # instagram
   get "/instagram/feed/:feed_id", to: "instagram#individual_feed", as: "instagram_feed"
-  get "/vimeo/feed/:feed_id", to: "vimeo#individual_feed", as: "vimeo_feed"
   post "/instagram/feed/:feed_id/subscribe", to: "instagram#subscribe", as: "instagram_subscribe"
+  delete "/instagram/feed/:feed_id/unsubscribe", to: "instagram#unsubscribe", as: "instagram_unsubscribe"
+  # vimeo
+  get "/vimeo/feed/:feed_id", to: "vimeo#individual_feed", as: "vimeo_feed"
   post "/vimeo/feed/:feed_id/subscribe", to: "vimeo#subscribe", as: "vimeo_subscribe"
+  delete "/vimeo/feed/:feed_id/unsubscribe", to: "vimeo#unsubscribe", as: "vimeo_unsubscribe"
 
-  # search routes
+  # search routes --------------------------------------------------------------
   post "/search", to: "welcome#search", as: "search"
   get "/instagram/:query", to: "instagram#results", as: "instagram_results"
   get "/vimeo/:query", to: "vimeo#results", as: "vimeo_results"
