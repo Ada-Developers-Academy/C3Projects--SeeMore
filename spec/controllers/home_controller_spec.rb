578761afb44b710c1afc79a6b1d89edea3ef5052
Carly Jugler <carjug@gmail.com>
2015-08-12 11:44:55 -0700

    Adds tests for home controller search method

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -8,22 +8,16 @@ RSpec.describe HomeController, type: :controller do
     session[:user_id] = @logged_user.id
   }
 
-  # will need to refactor using this setup for VCR use
-  # only on tests that would make an API call
-  # VCR.use_cassette('whatever cassette name you want') do
-  #    # the body of the test would go here...
-  # end
   describe "GET #index" do
+    it "renders the index page" do
+      get :index
+
+      expect(subject).to render_template :index
+      expect(response).to have_http_status(200)
+    end
+
     context "when logged in" do
-      it "renders the index page" do
-        log_in
-        get :index
-
-        expect(subject).to render_template :index
-        expect(response).to have_http_status(200)
-      end
-
-       it "user has access to their posts" do
+      it "user has access to their posts" do
         log_in
         sub = create(:ig_sub)
         post = create(:post)
@@ -32,17 +26,21 @@ RSpec.describe HomeController, type: :controller do
         get :index
 
         expect(@logged_user.posts).to include post
-       end
-    end
-
-    context "when not logged in" do
-      it "renders the index page" do
-        get :index
-
-        expect(subject).to render_template :index
-        expect(rendered).to match "You must sign in to feed the beast."
       end
     end
   end
 
+  # will need to refactor using this setup for VCR use
+  # only on tests that would make an API call
+  describe "twitter search" do
+    it "redirects to twitter results index" do
+      VCR.use_cassette('twitter search') do
+         get :search, { website: "twitter", search: "tinder" }
+
+         expect(response).to redirect_to twi_subscriptions_path(params: {twitter_search: "tinder"})
+         binding.pry
+         # expect(@results.first.screen_name).to eq "Tinder"
+       end
+     end
+  end
 end
