ca047d4722af723083d20aa875eec52910791d52
Brenna <brennaleker@gmail.com>
2015-08-12 14:11:45 -0700

    added update methods to cron schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 # in a for reals awesome app we would want this to be more frequent but due to the rate limits of both APIs we cannot update more frequently.
 every 2.hours do
-  runner "Feed.instagram_update_method"
-  runner "Feed.vimeo_update_method"
+  runner "Feed.update_vimeo_feed"
+  runner "Feed.update_instagram_feed"
 end
 
 # Learn more: http://github.com/javan/whenever
