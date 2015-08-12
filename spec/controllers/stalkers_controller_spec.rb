7d2309a07e55a3da317f0bbf0b29037aa7f58355
MisShellyMac <michellemccarthy@hotmail.com>
2015-08-11 18:49:12 -0700

    2 green rspec tests for stalkers_controller_spec.rb

--- spec/controllers/stalkers_controller_spec.rb
+++ spec/controllers/stalkers_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe StalkersController, type: :controller do
-  pending "Stalkers Controller Tests"
+  describe "GET index " do
+    it "redirects to landing_path if no user id is present" do
+      get :index, stalker_id: 3
+      expect(response).to redirect_to(landing_path)
+    end
+
+    before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]}
+    let!(:stalker) {Stalker.find_or_create_from_auth_hash(OmniAuth.config.mock_auth[:twitter])}
+    it "renders the dashboard_path if user is logged in" do
+     session[:stalker_id] = 1
+      get :index, stalker_id: 1
+      expect(response).to render_template :index
+    end
+  end
 end
