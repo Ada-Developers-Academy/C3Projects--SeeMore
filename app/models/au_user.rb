977095fcd32e2aa2a548aee2e60cd39d4baa6998
Brenna <brennaleker@gmail.com>
2015-08-12 14:09:22 -0700

    WIP- using instagram username in place of name

--- app/models/au_user.rb
+++ app/models/au_user.rb
@@ -2,13 +2,13 @@ class AuUser < ActiveRecord::Base
   has_and_belongs_to_many :feeds
   has_many :posts, through: :feeds
 
-  validates :uid, :provider, :name, :presence => true
+  validates :uid, :provider, :presence => true
 
   def self.create_with_omniauth(auth)
     create! do |au_user|
       au_user.provider = auth["provider"]
       au_user.uid = auth["uid"]
-      au_user.name = auth["info"]["name"]
+      au_user.name = auth["info"]["username"]
       au_user.email = auth["info"]["email"]
       if auth["info"]["pictures"]
         au_user.avatar = auth["info"]["pictures"][0]["link"]
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,25 +1,7 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-
-every 2.minutes do
-  runner "Instragram.instagram_update_method"
-  runner "Vimeo.vimeo_update_method"
+# in a for reals awesome app we would want this to be more frequent but due to the rate limits of both APIs we cannot update more frequently.
+every 2.hours do
+  runner "Feed.instagram_update_method"
+  runner "Feed.vimeo_update_method"
 end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
 
 # Learn more: http://github.com/javan/whenever
