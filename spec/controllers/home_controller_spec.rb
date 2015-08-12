70f905576f20cf0f9a9a0f1a6f7068e4c5688f11
Carly Jugler <carjug@gmail.com>
2015-08-12 11:02:47 -0700

    WIP home controller specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,48 @@
 require 'rails_helper'
 require 'support/vcr_setup'
+require 'pry'
 
 RSpec.describe HomeController, type: :controller do
+  let(:log_in) {
+    @logged_user = create :user
+    session[:user_id] = @logged_user.id
+  }
 
   # will need to refactor using this setup for VCR use
   # only on tests that would make an API call
   # VCR.use_cassette('whatever cassette name you want') do
   #    # the body of the test would go here...
   # end
+  describe "GET #index" do
+    context "when logged in" do
+      it "renders the index page" do
+        log_in
+        get :index
+
+        expect(subject).to render_template :index
+        expect(response).to have_http_status(200)
+      end
+
+       it "user has access to their posts" do
+        log_in
+        sub = create(:ig_sub)
+        post = create(:post)
+        @logged_user.subscriptions << sub
+
+        get :index
+
+        expect(@logged_user.posts).to include post
+       end
+    end
+
+    context "when not logged in" do
+      it "renders the index page" do
+        get :index
+
+        expect(subject).to render_template :index
+        expect(rendered).to match "You must sign in to feed the beast."
+      end
+    end
+  end
 
 end
