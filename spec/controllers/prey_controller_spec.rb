b59b8bb262038143e4f2b5c917b67f21dbeb0b4d
Corinne Pingul <corinnepingul@gmail.com>
2015-08-13 10:46:35 -0700

    Adds more tests for sessions#delete.

--- spec/controllers/prey_controller_spec.rb
+++ spec/controllers/prey_controller_spec.rb
@@ -8,6 +8,28 @@ RSpec.describe PreyController, type: :controller do
   # redirects to root path
   # works for both twitter and instagram
 
+  # describe "prey#create" do
+  #   let(:prey) { create(:prey) }
+  #
+  #   it "does not create a new prey if one is already in the db" do
+  #     # prey
+  #     post :create, { FactoryGirl.attributes_for(:prey)
+  #
+  #     expect(Prey.all.count).to eq 1
+  #   end
+  #
+  #   it "creates a new prey if one doesn't already exist" do
+  #     build(:prey)
+  #     post :create, FactoryGirl.attributes_for(:prey)
+  #
+  #     expect(Prey.first).to eq prey
+  #   end
+  #
+  #   it "redirects to root path" do
+  #
+  #   end
+  # end
+
   pending "tests for PreyController#unfollow"
   # flash messages
   # does not delete the prey from the db
