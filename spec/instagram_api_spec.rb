24bc41409c9323e5753cc7e0e1ed4109099a8a49
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-12 21:09:51 -0700

    added working test for #user_search; test breaks if different search term is used; fixed name of class

--- spec/instagram_api_spec.rb
+++ spec/instagram_api_spec.rb
@@ -2,7 +2,18 @@ require 'rails_helper'
 require 'instagram_api'
 require 'support/vcr_setup'
 
-RSpec.describe TwitterApi do
+RSpec.describe InstagramApi do
   let(:instagram_api) { InstagramApi.new }
+
+  # results.count will equal 4 if the search term is "rihanna"
+  # not sure why -- is this a known instagram bug?
+  describe "#user_search" do
+    it "returns up to a specified number of users" do
+      VCR.use_cassette 'instagram_api/user_search' do
+        results = instagram_api.user_search("beyonce", 3)
+        expect(results.count).to be <= 3
+      end
+    end
+  end
 
 end
