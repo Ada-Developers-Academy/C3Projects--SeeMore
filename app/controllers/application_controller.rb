f357002954b30b10cfe40767d10c74f81180a1a2
Jeri <drvonnjerryxlii@gmail.com>
2015-08-13 13:53:13 -0700

    fixing issue #55 Since we deleted developer strategy I think we can delete skip_before_action :verify_authenticity_token in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception # FIXME: these two callbacks are cancelling each other out
-  skip_before_action :verify_authenticity_token # FIXME: resolve verify_authenticity_token madness
   before_action :require_login
   helper_method :current_user
 
