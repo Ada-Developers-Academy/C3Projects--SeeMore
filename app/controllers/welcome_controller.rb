727371fedd31ed242b9095cfa42287bcbd2700be
Jeri <drvonnjerryxlii@gmail.com>
2015-08-13 13:56:31 -0700

    removing reset_session per issue #63

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,6 @@ class WelcomeController < ApplicationController
   def about; end
 
   def index
-    #reset_session # TODO: remove this before final PR. keep until no more rake db:reset
     if current_user
       posts = current_user.posts
       @posts = posts.only_thirty
