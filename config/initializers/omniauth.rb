3e0b66cf9490087fd4d42aa6648e0a670f333569
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-13 10:50:45 -0700

    Adds not production wrapper for developers authorization strategy.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer,
-    fields: [:name, :email],
-    uid_field: :email
+    if !Rails.env.production?
+      provider :developer,
+        fields: [:name, :email],
+        uid_field: :email
+    end
 
   provider :instagram, ENV['INSTAGRAM_ID'], ENV['INSTAGRAM_SECRET']
 end
