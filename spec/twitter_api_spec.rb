e05dbdff41eb7fc7b84cc365348794464f57b30b
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-12 18:10:23 -0700

    refactored specs to use local variables instead of instance; working on #get_new_posts spec WIP

--- spec/twitter_api_spec.rb
+++ spec/twitter_api_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe TwitterApi do
   describe "#user_search" do
     it "returns three users" do
       VCR.use_cassette 'twitter_api/user_search' do
-        @results = twitter_api.user_search("rihanna", 3)
-        expect(@results.count).to eq 3
+        results = twitter_api.user_search("rihanna", 3)
+        expect(results.count).to eq 3
       end
     end
   end
@@ -19,9 +19,35 @@ RSpec.describe TwitterApi do
 
     it "returns a blockquote" do
       VCR.use_cassette 'twitter_api/embed_html_without_js' do
-        @result = twitter_api.embed_html_without_js(tweet_id)
-        expect(@result[1..10]).to eq "blockquote"
+        result = twitter_api.embed_html_without_js(tweet_id)
+        expect(result[1..10]).to eq "blockquote"
       end
     end
   end
+
+  # def get_posts(id, last_post_id)
+  #   client.user_timeline(id, timeline_options(last_post_id))
+  # end
+
+  ###### this spec is currently broken
+  # describe "#get_posts" do
+  #   context "last_post_id present" do
+  #   let(:followee) { create :followee }
+
+  #     it "gets a collection of posts for a followee" do
+  #       VCR.use_cassette 'twitter_api/get_posts_last_post_id_present' do
+  #         # binding.pry
+  #         id = followee.id
+  #         last_post_id = followee.last_post_id
+  #         @posts = twitter_api.get_posts(id, last_post_id)
+  #         expect(@posts.count).to_be > 0
+  #       end
+  #     end
+  #   end
+
+  #   context "last_post_id absent" do
+  #     it "gets 1 post for a followee" do
+  #     end
+  #   end
+  # end
 end
