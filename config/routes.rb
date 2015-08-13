310e83e949cc4aace19c5ccd90cdc120471d609b
Anita <asovay@gmail.com>
2015-08-13 14:30:00 -0700

    added custom error messages

--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,8 @@ Rails.application.routes.draw do
   resources :instagrams, only: [:create, :destroy]
   resources :tweets, only: [:create, :destroy]
 
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
 end
