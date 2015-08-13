4ce2245d5466d8ee036ca66b13b64af1b868e3bf
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-12 20:58:27 -0700

    updated #user_search to match <= USER_COUNT

--- spec/twitter_api_spec.rb
+++ spec/twitter_api_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe TwitterApi do
   let(:twitter_api) { TwitterApi.new }
 
   describe "#user_search" do
-    it "returns three users" do
+    it "returns up to a specified number of users" do
       VCR.use_cassette 'twitter_api/user_search' do
         results = twitter_api.user_search("rihanna", 3)
-        expect(results.count).to eq 3
+        expect(results.count).to be <= 3
       end
     end
   end
