c1395343497f4fed05cacd5c8d6a1be398b00f6f
Jeri <drvonnjerryxlii@gmail.com>
2015-08-13 10:09:50 -0700

    wip cron task

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,9 @@
-# in a for reals awesome app we would want this to be more frequent but due to
-# the rate limits of both APIs we cannot update more frequently.git aff
-every 2.hours do
-  runner """
-  feeds = Feed.all
-  feeds.each do |feed|
-    feed.update_feed
-  end
-  """
-end
+# since Vimeo does not publicly list their rate limits, we're basing these
+# numbers off the Instagram rate limits, which are 5,000 API calls per hour.
+# that works out to 83 calls per minute. in the worst case scenario, with the
+# numbers below we'll be using 4 calls per minute for updating our cached feeds.
+# that leaves 79 for users of our site searching for new threads to subscribe to.
 
-# Learn more: http://github.com/javan/whenever
+every 12.hours do # for the purposes of this test, we're changing the time to 5 seconds and 9:45
+  rake "update_feeds:update"
+end
